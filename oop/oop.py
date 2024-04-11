720ef8d7e4aa62f3ef45e6a98dbe089a602359c2
stark5200 <a.taha5200@gmail.com>
2024-04-11 20:45:11 +0200

    short commit message

--- oop/oop.py
+++ oop/oop.py
@@ -1,5 +1,9 @@
 # main function goes here
-
+# some git commands:
+# git push --force to force changes
+# git status --short to get short message about current state
+# git commit -a to commit changes and skipping staging
+#
 def oop():
   max = 10
   return max
