cea57629bfa8822782ba898bb3306f1769aa891b
stark5200 <a.taha5200@gmail.com>
2024-04-12 22:05:15 +0200

    destroy wall

--- oop/oop.py
+++ oop/oop.py
@@ -1,9 +1,13 @@
 # main function goes here
 # some git commands:
-# git push --force to force changes
+# git push --force/--force-with-lease to force changes
 # git status --short to get short message about current state
 # git commit -a to commit changes and skipping staging
-#
-def oop():
-  max = 10
-  return max
+# git reset (--soft --mixed --hard) <hash of A>   resets current commit to old commit
+# In general, soft: stage everything, mixed: unstage everything, hard: ignore everything up to the commit I'm resetting from
+
+def destroy_wall(wall_health):
+    for w in wall_health:
+        if w <= 0:
+            wall_health.remove(w)
+    return wall_health
