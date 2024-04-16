0dc6365b7d9ce86d454f1c5474eea6b4d5514903
stark5200 <a.taha5200@gmail.com>
2024-04-16 07:54:46 +0200

    multi inheritance

--- oop/Inheritance/main.py
+++ oop/Inheritance/main.py
@@ -16,3 +16,18 @@ class Archer(Human):
 
     def get_num_arrows(self):
         return self.__num_arrows
+
+    def use_arrows(self, num):
+        if self.__num_arrows < num:
+            raise Exception("not enough arrows")
+        self.__num_arrows -= num
+
+
+class Crossbowman(Archer):
+    def __init__(self, name, num_arrows):
+        super().__init__(name, num_arrows)
+
+    def triple_shot(self, target):
+        self.use_arrows(3)
+        return f"{target.get_name()} was shot by 3 crossbow bolts"
+
