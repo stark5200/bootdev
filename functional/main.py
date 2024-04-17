fed8cf7f7030fb4153b053001c2e51dff66dfbe2
stark5200 <a.taha5200@gmail.com>
2024-04-17 22:04:35 +0200

    functional

---
+++ functional/main.py
@@ -0,0 +1,18 @@
+def stylize_title(document):
+    pass
+
+
+# Don't touch below this line
+
+
+def center_title(document):
+    width = 40
+    title = document.split("\n")[0]
+    centered_title = title.center(width)
+    return document.replace(title, centered_title)
+
+
+def add_border(document):
+    title = document.split("\n")[0]
+    border = "*" * len(title)
+    return document.replace(title, title + "\n" + border)
