8fc3734b5ff36c69a2745c8c3eba7d0f20bf1b05
stark5200 <a.taha5200@gmail.com>
2024-04-20 14:17:54 +0200

    using filter

--- functional/lambda.py
+++ functional/lambda.py
@@ -58,3 +58,18 @@ def convert_line(line):
     if len(line) > 0 and line[0] == old_bullet:
         return new_bullet + line[1:]
     return line
+  
+
+#The built-in filter function takes a function and an iterable (in this case a list) and returns a new iterable that only contains elements from the original iterable where the result of the function on that item returned True.
+
+def is_even(x):
+    return x % 2 == 0
+
+numbers = [1, 2, 3, 4, 5, 6]
+evens = list(filter(is_even, numbers))
+print(evens)
+# [2, 4, 6]
+
+# remove "-" lines using filter
+def remove_invalid_lines(document):
+    return "\n".join(list(filter(lambda x: not(x.startswith("-")), document.split("\n"))))
