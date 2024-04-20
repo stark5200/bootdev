bc52b2f9cda024a27494c79b42df753e673004b3
stark5200 <a.taha5200@gmail.com>
2024-04-20 20:44:07 +0200

    paired

--- functional/reduce/main.py
+++ functional/reduce/main.py
@@ -10,3 +10,26 @@ def accumulate_first_sentences(sentences, n):
     elif len(sentences) == 0:
         return ""
     return functools.reduce(accumulate, sentences[:n]) + "."
+
+#zip    
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+c = list(zip(a, b))
+print(c)
+# [(1, 4), (2, 5), (3, 6)]
+
+# 
+
+valid_formats = [
+    "docx",
+    "pdf",
+    "txt",
+    "pptx",
+    "ppt",
+    "md",
+]
+def pair_document_with_format(doc_names, doc_formats):
+    paired = list(zip(doc_names, doc_formats))
+    is_valid = lambda x: x[1] in valid_formats
+    return filter(is_valid, paired)
