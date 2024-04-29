3c586ffc84ccc949de722aa03d991cada9f1e27a
stark5200 <a.taha5200@gmail.com>
2024-04-29 20:52:33 +0200

    function transform

--- functional/pure_functions/main.py
+++ functional/pure_functions/main.py
@@ -167,6 +167,47 @@ def reverse_string(s):
     if s == "":
       return s
     return(s[-1]+reverse_string(s[:-1]))
+  
+def get_logger(formatter):
+    # ?
+
+
+# Don't edit below this line
+
+
+def test(first, errors, formatter):
+    print("Logs:")
+    logger = get_logger(formatter)
+    for err in errors:
+        logger(first, err)
+    print("====================================")
+
+
+def colon_delimit(first, second):
+    return f"{first}: {second}"
+
+
+def dash_delimit(first, second):
+    return f"{first} - {second}"
+
+
+def fTransform():
+    db_errors = [
+        "out of memory",
+        "cpu is pegged",
+        "networking issue",
+        "invalid syntax",
+    ]
+    test("Doc2Doc FATAL", db_errors, colon_delimit)
+
+    mail_errors = [
+        "email too large",
+        "non alphanumeric symbols found",
+    ]
+    test("Doc2Doc WARNING", mail_errors, dash_delimit)
+
+
+fTransform()
 
  
 
