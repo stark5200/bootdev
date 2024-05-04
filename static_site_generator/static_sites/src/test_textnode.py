87146c0140a51b754fa05485b3406e5e55969e1a
stark5200 <a.taha5200@gmail.com>
2024-05-04 23:33:25 +0200

    unit tests with failure

--- static_site_generator/static_sites/src/test_textnode.py
+++ static_site_generator/static_sites/src/test_textnode.py
@@ -5,9 +5,19 @@ from textnode import TextNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
+        node1 = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
-        self.assertEqual(node, node2)
+        self.assertEqual(node1, node2)
+    
+    def test_eq2(self):
+        node3 = TextNode("2nd test with default url", "italic", "google.com")
+        node4 = TextNode("2nd test with default url", "italic")
+        self.assertEqual(node3, node4)
+
+    def test_eq3(self):
+        node5 = TextNode("my twisted dark fantsy", "bold", "boot.dev")
+        node6 = TextNode("by kanye west", "crazy", "crashcourse.com")
+        self.assertEqual(node5, node6)
 
 
 if __name__ == "__main__":
