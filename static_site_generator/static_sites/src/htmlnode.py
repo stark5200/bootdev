10b59dc05fa54c97842073f73b7eb56ce8933f00
stark5200 <a.taha5200@gmail.com>
2024-05-05 00:04:41 +0200

    htmlnode test

--- static_site_generator/static_sites/src/htmlnode.py
+++ static_site_generator/static_sites/src/htmlnode.py
@@ -1,6 +1,18 @@
 class HTMLNode:
-  def __init__(self, tag, value, children, props):
+  def __init__(self, tag=None, value=None, children=None, props=None):
     self.tag = tag
     self.value = value
     self.children = children
     self.props = props
+    
+  def to_html(self):
+    raise NotImplementedError
+  
+  def props_to_html(self):
+    if self.props:
+      self_props = ''
+      for key, value in self.props:
+        self_props += f' {key}="{value}"'
+        
+  def __repr__(self):
+    print(f"Tag: {self.tag}\n Value: {self.value}\n Children: {self.children}\n Props:{self.props}")
