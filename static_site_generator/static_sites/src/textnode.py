0ab6b89819bd4eea04cee68b3ffb4dcf32efa868
stark5200 <a.taha5200@gmail.com>
2024-05-07 00:36:11 +0200

    chapter 2 done

--- static_site_generator/static_sites/src/textnode.py
+++ static_site_generator/static_sites/src/textnode.py
@@ -1,6 +1,21 @@
+from leafnode import LeafNode
+
 class TextNode:
+  text_type_text = "text"
+  text_type_bold = "bold"
+  text_type_italic = "italic"
+  text_type_code = "code"
+  text_type_link = "link"
+  text_type_image = "image"
   
-  text_types = ["text", "bold", "italic", "code", "link", "image"]
+  text_types = [
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image
+  ]
   
   def __init__(self, text, text_type, url="google.com"):
     self.text = text
@@ -8,8 +23,20 @@ class TextNode:
     self.url = url
     
   def text_node_to_html_node(self, text_node):
-    if text_node not in self.text_types :
+    if text_node.text_type not in self.text_types :
       raise Exception("text node type not valid")
+    if text_node.text_type == self.text_type_text:
+      return LeafNode(None, self.text)
+    if text_node.text_type == self.text_type_bold:
+      return LeafNode("b", self.text)
+    if text_node.text_type == self.text_type_italic:
+      return LeafNode("i", self.text)
+    if text_node.text_type == self.text_type_code:
+      return LeafNode("code", self.text)
+    if text_node.text_type == self.text_type_link:
+      return LeafNode("a", self.text, {"href": self.url})
+    if text_node.text_type == self.text_type_image:
+      return LeafNode(tag="img", value="", props={"src": self.url, "alt":self.text})
     
   def __eq__(self, textNode):
     return (self.text == textNode.text and self.text_type == textNode.text_type and self.url == textNode.url)
