b3ce0b722e694cf7e8d6acaa738037deecf129e9
stark5200 <a.taha5200@gmail.com>
2024-05-06 18:27:45 +0200

    text types

--- static_site_generator/static_sites/src/textnode.py
+++ static_site_generator/static_sites/src/textnode.py
@@ -1,8 +1,15 @@
 class TextNode:
+  
+  text_types = ["text", "bold", "italic", "code", "link", "image"]
+  
   def __init__(self, text, text_type, url="google.com"):
     self.text = text
     self.text_type = text_type
     self.url = url
+    
+  def text_node_to_html_node(self, text_node):
+    if text_node not in self.text_types :
+      raise Exception("text node type not valid")
     
   def __eq__(self, textNode):
     return (self.text == textNode.text and self.text_type == textNode.text_type and self.url == textNode.url)
