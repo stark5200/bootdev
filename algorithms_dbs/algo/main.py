cbd8f6ce2f3906b18662bce53ea72dd1f4fc4914
stark5200 <a.taha5200@gmail.com>
2024-05-20 00:04:28 +0200

    some weird bug

--- algorithms_dbs/algo/main.py
+++ algorithms_dbs/algo/main.py
@@ -12,3 +12,30 @@ def find_last_name(names_dict, first_name):
         if first_name in names_dict:
             return names_dict[first_name]
     return None
+  
+def binary_search(target, arr):
+    if arr == None or target == None  or len(arr) == 0:
+        return False
+    n = len(arr)
+    if n == 1:
+      if arr[0] == target:
+        return True
+      return False
+    low, high = 0, n-1
+    if target < arr[low] or target > arr[high]:
+        return False
+    median = low + high // 2
+    while low <= high:
+        if arr[median] == target:
+            return True
+        if target < arr[median]:
+            high = median
+        if target > arr[median]:
+            low = median
+        median = (low + high) // 2
+        print("target: " + str(target) + ", low:" + str(low) + ", median:" + str(median) + ", high:" + str(high) + ".")
+    return False
+    
+
+    
+
