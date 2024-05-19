85e085abaa565ef2fe18dd5cf94297d9dbb4286a
stark5200 <a.taha5200@gmail.com>
2024-05-19 23:34:13 +0200

    name in dict

--- algorithms_dbs/algo/main.py
+++ algorithms_dbs/algo/main.py
@@ -6,3 +6,9 @@ def get_avg_brand_followers(all_handles, brand_name):
             if brand_name in follower:
                 brand_count+=1
     return brand_count/list_count
+  
+def find_last_name(names_dict, first_name):
+    if first_name:
+        if first_name in names_dict:
+            return names_dict[first_name]
+    return None
