d4a97d8164e7cb7bb0eefebd0d19e1369eda3423
stark5200 <a.taha5200@gmail.com>
2024-05-21 04:10:50 +0200

    simulate stack

--- algorithms_dbs/datastructure/main.py
+++ algorithms_dbs/datastructure/main.py
@@ -1,5 +1,8 @@
 # Fantasy Quest
 
+# Calling a function pushes a call frame onto the runtime stack
+# Returning from a function pops the top frame off the stack.
+
 def count_potions(inventory):
     count = 0
     for item in inventory:
@@ -28,3 +31,64 @@ class Stack:
 
     def size(self):
         return len(self.arrows)
+
+# Simulating Function call stack
+
+def attack_action():
+    call(shoot_arrow)
+    call(calc_new_health)
+
+
+def shoot_arrow():
+    call(calc_damage)
+
+
+def calc_damage():
+    call(apply_damage)
+
+
+# don't touch below this line
+
+
+def calc_new_health():
+    pass
+
+
+def apply_damage():
+    pass
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if len(self.items) == 0:
+            return None
+        return self.items[len(self.items) - 1]
+
+
+stack = Stack()
+
+
+def call(func):
+    stack.push(func.__name__)
+    print("Pushing " + func.__name__)
+    print("Stack: " + str(stack.items))
+    print("=================================")
+    func()
+    stack.pop()
+    print("Popping " + func.__name__)
+    print("Stack: " + str(stack.items))
+    print("=================================")
+
+
+call(attack_action)
