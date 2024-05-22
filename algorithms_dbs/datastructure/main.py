d414c8e8bba00fd64b412fb58d7f74af38d2f03e
stark5200 <a.taha5200@gmail.com>
2024-05-22 06:18:14 +0200

    Linked List

--- algorithms_dbs/datastructure/main.py
+++ algorithms_dbs/datastructure/main.py
@@ -158,3 +158,43 @@ class Queue:
 
     def size(self):
         return len(self.items)
+      
+      
+# Linked list
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node != None:
+            yield node
+            node = node.next
+
+    # don't touch below this line
+
+    def __repr__(self):
+        nodes = []
+        current = self.head
+        while current and hasattr(current, "val"):
+            nodes.append(current.val)
+            current = current.next
+        return " -> ".join(nodes)
+
+    def add_to_tail(self, node):
+        # Simple for now, will get updated
+        self.head = node
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def set_next(self, node):
+        self.next = node
+
+    def __repr__(self):
+        return self.val
+
