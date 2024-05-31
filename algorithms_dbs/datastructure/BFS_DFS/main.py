d7880b664bd9b10840bc77a64b30dc58ecfcac2c
stark5200 <a.taha5200@gmail.com>
2024-06-01 00:21:28 +0200

    bfs dfs 1

---
+++ algorithms_dbs/datastructure/BFS_DFS/main.py
@@ -0,0 +1,35 @@
+class Graph:
+    def breadth_first_search(self, v):
+        visited = []
+        to_visit = []
+        to_visit.append(v)
+        while len(to_visit) != 0:
+            visited.append(to_visit.pop())
+            sorted_neighbor = sorted(self.graph[visited[-1]])
+            for i in sorted_neighbor:
+                if ((i not in visited) and (i not in to_visit)):
+                    to_visit.append(i)
+        return visited
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u in self.graph.keys():
+            self.graph[u].add(v)
+        else:
+            self.graph[u] = set([v])
+        if v in self.graph.keys():
+            self.graph[v].add(u)
+        else:
+            self.graph[v] = set([u])
+
+    def __repr__(self):
+        result = ""
+        for key in self.graph.keys():
+            result += f"Vertex: '{key}'\n"
+            for v in sorted(self.graph[key]):
+                result += f"has an edge leading to --> {v} \n"
+        return result
