8238df53589de3324f79c1855eddaeb456fa2bdf
stark5200 <a.taha5200@gmail.com>
2024-06-08 23:47:05 +0200

    8 Cells drawn

--- maze/graphics.py
+++ maze/graphics.py
@@ -10,10 +10,10 @@ class Window:
         self.root_widget.geometry(str(self.width)+"x"+str(self.height))
         self.label = Label(self.root_widget, text="Maze 1!", font=("Arial", 18))
         self.label.pack(padx=20, pady=20)
-        self.canvas_widget = Canvas(self.root_widget, bg="white", height=self.height, width=self.width)
+        self.canvas_widget = Canvas(self.root_widget, bg="White", height=self.height, width=self.width)
         self.canvas_widget.pack(fill=BOTH, expand=1)
         self.is_running = False
-        self.root_widget.mainloop()
+        #self.root_widget.mainloop()
         print("window opened...")
         self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
         
@@ -52,5 +52,38 @@ class Line:
         
     def draw(self, canvas, fill_color):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        
+        
+class Cell:
+    def __init__(self, x1, y1, x2, y2, window):
+        # Takes topleft and bottomright coordinates
+        # self.cell_size = 10
+        
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bot_wall = True
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.window = window
+        
+    def draw(self):
+        if self.has_left_wall:
+            self.window.canvas_widget.create_line(self.x1, self.y1, self.x1, self.y2, fill="Black", width=2)
+            
+        if self.has_right_wall:
+            self.window.canvas_widget.create_line(self.x2, self.y1, self.x2, self.y2, fill="Black", width=2)
+            
+        if self.has_top_wall:
+            self.window.canvas_widget.create_line(self.x1, self.y1, self.x2, self.y1, fill="Black", width=2)
+            
+        if self.has_bot_wall:
+            self.window.canvas_widget.create_line(self.x1, self.y2, self.x2, self.y2, fill="Black", width=2)
+            
+        
+
+  
       
         
--- maze/main.py
+++ maze/main.py
@@ -1,5 +1,5 @@
 # Maze project
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
         
 
 def main():
@@ -7,6 +7,40 @@ def main():
     point_1 = Point(15, 20)
     point_2 = Point(500, 200)
     line_1 = Line(point_1, point_2)
+    
+    cell_1 = Cell(30, 30, 50, 50, win)
+    cell_1.has_bot_wall = False
+    cell_1.draw()
+    cell_2 = Cell(60, 30, 80, 50, win)
+    cell_2.has_left_wall = False
+    cell_2.draw()
+    cell_3 = Cell(90, 30, 110, 50, win)
+    cell_3.has_top_wall = False
+    cell_3.draw()
+    cell_4 = Cell(120, 30, 140, 50, win)
+    cell_4.has_right_wall = False
+    cell_4.draw()
+    
+    cell_5 = Cell(30, 60, 50, 80, win)
+    cell_5.has_top_wall = False
+    cell_5.has_right_wall = False
+    cell_5.draw()
+    cell_6 = Cell(60, 60, 80, 80, win)
+    cell_6.has_bot_wall = False
+    cell_6.has_top_wall = False
+    cell_6.draw()
+    cell_7 = Cell(90, 60, 110, 80, win)
+    cell_7.has_right_wall = False
+    cell_7.has_left_wall = False
+    cell_7.has_top_wall = False
+    cell_7.draw()
+    cell_8 = Cell(120, 60, 140, 80, win)
+    cell_8.has_bot_wall = False
+    cell_8.has_right_wall = False
+    cell_8.has_left_wall = False
+    cell_8.has_top_wall = False
+    cell_8.draw()
+    
     win.draw_line(line_1, "Red")
     win.wait_for_close()
     
