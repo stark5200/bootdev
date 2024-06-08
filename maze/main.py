cbb4fd7d0e160e7cb9eb2853e8ebe505cbed9fc5
stark5200 <a.taha5200@gmail.com>
2024-06-08 20:13:21 +0200

    window opened

--- maze/main.py
+++ maze/main.py
@@ -1,11 +1,19 @@
 # Maze project
-from tkinter import Tk, BOTH, Canvas
+from tkinter import Tk, BOTH, Canvas, Label
 class Window:
   
     def __init__(self, width, height):
         root_widget = Tk()
         root_widget.title = "Window 1"
+        root_widget.geometry(str(width)+"x"+str(height))
+        label = Label(root_widget, text="Maze 1!", font=("Arial", 18))
+        label.pack(padx=20, pady=20)
         c_widget = Canvas()
+        c_widget.pack()
+        is_running = False
+        root_widget.mainloop()
+        
+window1 = Window(400, 400)
         
 
     
