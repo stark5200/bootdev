1ef54acd0863599a2450a1b7aac512614a959556
stark5200 <a.taha5200@gmail.com>
2024-06-20 20:42:21 +0200

    textbox

--- maze/tkyt.py
+++ maze/tkyt.py
@@ -6,3 +6,7 @@ root.geometry("500x500")
 root.title("My yt Gui")
 
 label = tk.Label(root, text="Hello World!", font=("Arial", 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, height=3, font=("Arial", 16))
+textbox.pack()
