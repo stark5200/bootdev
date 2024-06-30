1e853db6ff72bd89615c43295baf1690504f813f
stark5200 <a.taha5200@gmail.com>
2024-06-30 19:18:00 +0200

    more columns

--- maze/tkyt.py
+++ maze/tkyt.py
@@ -5,13 +5,14 @@ window = tk.Tk()
 window.geometry("500x500")
 window.title("My yt Gui")
 
+#place objects with pack, grid or place
 label = tk.Label(window, text="Hello World!", font=("Arial", 18))
 label.pack(padx=20, pady=20)
 
 textbox = tk.Text(window, height=3, font=("Arial", 16))
 textbox.pack(padx=10)
 
-my_entry = tk.Entry(window)
+my_entry = tk.Entry(window) # entry is Text box with height 1
 my_entry.pack() 
 
 button = tk.Button(window, text="Click Me!", font=("Arial", 18))
@@ -22,7 +23,22 @@ buttonframe.columnconfigure(0, weight=1)
 buttonframe.columnconfigure(1, weight=1)
 buttonframe.columnconfigure(2, weight=1)
 
-btn1 = tk.Button(buttonframe, text="1", font=("Arial", 18))
-btn1.grid(row=0, column=0, sticky="news")
+btn1 = tk.Button(buttonframe, text="1", font=("Arial", 18)) # first param is master usually root or window, in this case the button frame
+btn1.grid(row=0, column=0, sticky="news") # news = tk.N+tk.E+tk.W+tk.S north east west south
+
+btn2 = tk.Button(buttonframe, text="2", font=("Arial", 18))
+btn2.grid(row=0, column=1, sticky="news") 
+
+btn3 = tk.Button(buttonframe, text="3", font=("Arial", 18))
+btn3.grid(row=0, column=2, sticky="news") 
+
+btn4 = tk.Button(buttonframe, text="4", font=("Arial", 18))
+btn4.grid(row=1, column=0, sticky="news") 
+
+btn5 = tk.Button(buttonframe, text="5", font=("Arial", 18))
+btn5.grid(row=1, column=1, sticky="news") 
+
+btn6 = tk.Button(buttonframe, text="6", font=("Arial", 18))
+btn6.grid(row=1, column=2, sticky="news") 
 
 window.mainloop()
