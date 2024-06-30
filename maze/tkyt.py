6081a67bba207b2b209b1f5e9a5fb5ede6161993
stark5200 <a.taha5200@gmail.com>
2024-06-30 19:05:22 +0200

    replace root with window

--- maze/tkyt.py
+++ maze/tkyt.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 
-root = tk.Tk()
+window = tk.Tk()
 
-root.geometry("500x500")
-root.title("My yt Gui")
+window.geometry("500x500")
+window.title("My yt Gui")
 
-label = tk.Label(root, text="Hello World!", font=("Arial", 18))
+label = tk.Label(window, text="Hello World!", font=("Arial", 18))
 label.pack(padx=20, pady=20)
 
-textbox = tk.Text(root, height=3, font=("Arial", 16))
+textbox = tk.Text(window, height=3, font=("Arial", 16))
 textbox.pack(padx=10)
 
-my_entry = tk.Entry(root)
+my_entry = tk.Entry(window)
 my_entry.pack() 
 
-button = tk.Button(root, text="Click Me!", font=("Arial", 18))
+button = tk.Button(window, text="Click Me!", font=("Arial", 18))
 button.pack(padx=10, pady=10)
 
-buttonframe = tk.Frame(root)
+buttonframe = tk.Frame(window)
 buttonframe.columnconfigure(0, weight=1)
 buttonframe.columnconfigure(1, weight=1)
 buttonframe.columnconfigure(2, weight=1)
@@ -25,4 +25,4 @@ buttonframe.columnconfigure(2, weight=1)
 btn1 = tk.Button(buttonframe, text="1", font=("Arial", 18))
 btn1.grid(row=0, column=0, sticky="news")
 
-root.mainloop()
+window.mainloop()
