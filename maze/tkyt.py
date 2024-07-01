6cbac14ca4cba08b3a71e4ea718aa6253abc75aa
stark5200 <a.taha5200@gmail.com>
2024-07-01 21:02:54 +0200

    int var

--- maze/tkyt.py
+++ maze/tkyt.py
@@ -61,7 +61,9 @@ class  MyGUI:
     self.textbox = tk.Text(self.root, height=5, font=("Arial", 16))
     self.textbox.pack(padx=10, pady=10)
     
-    self.check = tk.Checkbutton(self.root, text="Show Message", font=("Arial", 16))
+    self.check_state = tk.IntVar()
+    
+    self.check = tk.Checkbutton(self.root, text="Show Message", font=("Arial", 16), variable=self.check_state)
     self.check.pack(padx=10, pady=10)
     
     self.root.mainloop()
