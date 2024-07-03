5c6b9ef8c0f7b0f0219f30ecb5a1385e955496de
stark5200 <a.taha5200@gmail.com>
2024-07-03 22:16:46 +0200

    message cost

--- go/textio.go
+++ go/textio.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	// single-line comments start with "//"
-	// comments are just for documentation - they don't execute
-	fmt.Println("starting Textio server...")
+	messagesFromDoris := []string{
+		"You doing anything later??",
+		"Did you get my last message?",
+		"Don't leave me hanging...",
+		"Please respond I'm lonely!",
+	}
+	numMessages := float64(len(messagesFromDoris))
+	costPerMessage := .02
+
+	// don't touch above this line
+
+	totalCost := costPerMessage * numMessages
+
+	// don't touch below this line
+
+	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
 }
