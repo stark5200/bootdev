fdf5e0e59ed4c3912c0449fff49b895b8cd14950
stark5200 <a.taha5200@gmail.com>
2024-07-14 05:24:58 +0300

    mothly price

--- go/main.go
+++ go/main.go
@@ -1,8 +1,15 @@
 package main
 
 func getMonthlyPrice(tier string) int {
+	price := 0
 	if tier == "basic" {
-		return 100
+		price = 100
 	}
-	return 200
+	if tier == "premium" {
+		price = 150
+	}
+	if tier == "enterprise" {
+		price = 500
+	}
+	return price*100
 }
