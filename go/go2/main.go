86e0252ae7308ab06339386575a8750d1fee20b4
stark5200 <a.taha5200@gmail.com>
2024-07-17 07:14:19 +0300

    guard clauses

--- go/go2/main.go
+++ go/go2/main.go
@@ -13,3 +13,24 @@ func getBillForMonth(costPerSend, messagesSent int) (bill int) {
 	bill = costPerSend * messagesSent
 	return
 }
+
+func getInsuranceAmount(status insuranceStatus) int {
+  amount := 0
+  if !status.hasInsurance(){
+    amount = 1
+  } else {
+    if status.isTotaled(){
+      amount = 10000
+    } else {
+      if status.isDented(){
+        amount = 160
+        if status.isBigDent(){
+          amount = 270
+        }
+      } else {
+        amount = 0
+      }
+    }
+  }
+  return amount
+}
