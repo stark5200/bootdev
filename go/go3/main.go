8e1f80c372a6d1b187d22af26d14c273d1ca3c6d
stark5200 <a.taha5200@gmail.com>
2024-07-19 01:30:46 +0300

    anonymous

--- go/go3/main.go
+++ go/go3/main.go
@@ -6,3 +6,24 @@ func reformat(message string, formatter func(string) string) string {
 	thirdFormat := formatter(secondFormat)
 	return "TEXTIO: "+thirdFormat
 }
+
+func conversions(converter func(int) int, x, y, z int) (int, int, int) {
+	convertedX := converter(x)
+	convertedY := converter(y)
+	convertedZ := converter(z)
+	return convertedX, convertedY, convertedZ
+}
+
+func main() {
+	newX1, newY1, newZ1 := conversions(func(a int) int {
+	    return a + a
+	}, 1, 2, 3)
+	// newX is 2, newY is 4, newZ is 6
+
+	newX2, newY2, newZ2 := conversions(func(a int) int {
+	    return a * a
+	}, 1, 2, 3)
+	// newX is 1, newY is 4, newZ is 9
+
+	print(newX1, newY1, newZ1, newX2, newY2, newZ2)
+}
