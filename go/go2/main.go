d7f6b6716a0819b263f9422331503244a5e052a9
stark5200 <a.taha5200@gmail.com>
2024-07-18 23:48:44 +0300

    early returns

--- go/go2/main.go
+++ go/go2/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) (increase int) {
 	var lastMonthBill int
 	var thisMonthBill int
@@ -14,23 +18,32 @@ func getBillForMonth(costPerSend, messagesSent int) (bill int) {
 	return
 }
 
-func getInsuranceAmount(status insuranceStatus) int {
-  amount := 0
-  if !status.hasInsurance(){
-    amount = 1
-  } else {
-    if status.isTotaled(){
-      amount = 10000
-    } else {
-      if status.isDented(){
-        amount = 160
-        if status.isBigDent(){
-          amount = 270
-        }
-      } else {
-        amount = 0
-      }
-    }
-  }
-  return amount
+func calculator(a, b int) (mul, div int, err error) {
+	if b == 0 {
+		return 0, 0, errors.New("Can't divide by zero")
+	}
+	mul = a * b
+	div = a / b
+	return mul, div, nil
 }
+
+/*
+EARLY RETURNS
+Go supports the ability to return early from a function. This is a powerful feature that can clean up code, especially when used as guard clauses.
+
+Guard Clauses leverage the ability to return early from a function (or continue through a loop) to make nested conditionals one-dimensional. Instead of using if/else chains, we just return early from the function at the end of each conditional block.
+*/
+
+func divide(dividend, divisor int) (int, error) {
+	if divisor == 0 {
+		return 0, errors.New("Can't divide by zero")
+	}
+	return dividend/divisor, nil
+}
+
+/*
+Error handling in Go naturally encourages developers to make use of guard clauses. When I started writing more JavaScript, I was disappointed to see how many nested conditionals existed in the code I was working on.
+
+Let’s take a look at an exaggerated example of nested conditional logic:
+*/
+
