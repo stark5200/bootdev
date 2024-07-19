a8802ac6f90bd71362f3187928b061336da80722
stark5200 <a.taha5200@gmail.com>
2024-07-19 03:15:34 +0300

    challenge 1

--- go/challenge1/main.go
+++ go/challenge1/main.go
@@ -1,11 +1,18 @@
 package main
 
 func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
-	// ?
+	return costPerMessage * float64(numMessages) * calculateDiscount(numMessages)
 }
 
 func calculateDiscount(messagesSent int) float64 {
-	// ?
+	discount := 0
+	if messagesSent > 1000 {
+		discount = 10
+	}
+	if messagesSent > 5000 {
+		discount = 20
+	}
+	return float64(float64(100 - discount)/float64(100))
 }
 
 // don't touch below this line
