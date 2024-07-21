bb511c803642f75dd956b2c89e09c8c55a13114f
stark5200 <a.taha5200@gmail.com>
2024-07-21 23:47:36 +0300

    example

--- go/struct/main.go
+++ go/struct/main.go
@@ -11,3 +11,26 @@ func getBasicAuth(info authenticationInfo) formatted string {
 	formatted := "Authorization: Basic " + authenticationInfo.username + ":" + authenticationInfo.password
 	return
 }
+
+
+/// example
+
+type rect struct {
+  width int
+  height int
+}
+
+// area has a receiver of (r rect)
+// rect is the struct
+// r is the placeholder
+func (r rect) area() int {
+  return r.width * r.height
+}
+
+var r = rect{
+  width: 5,
+  height: 10,
+}
+
+fmt.Println(r.area())
+// prints 50
