e234782c8c3e147934dc22ea0c918e463263cae1
stark5200 <a.taha5200@gmail.com>
2024-07-22 20:51:23 +0300

    some git info

---
+++ go/git/main.go
@@ -0,0 +1,11 @@
+/*
+In Git, commands are divided into high-level ("porcelain") commands and low-level ("plumbing") commands. The porcelain commands are the ones that you will use most often as a developer to interact with your code. Some porcelain commands are:
+
+git status
+git add
+git commit
+git push
+git pull
+git log
+
+*/
