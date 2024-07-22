4317d9d54a91bfb7d22bc91399b1fb6664a00b1c
stark5200 <a.taha5200@gmail.com>
2024-07-22 20:52:12 +0300

    update

--- go/git/main.go
+++ go/git/main.go
@@ -1,4 +1,6 @@
 /*
+
+PORCELAIN AND PLUMBING
 In Git, commands are divided into high-level ("porcelain") commands and low-level ("plumbing") commands. The porcelain commands are the ones that you will use most often as a developer to interact with your code. Some porcelain commands are:
 
 git status
@@ -7,5 +9,11 @@ git commit
 git push
 git pull
 git log
+Don't worry about what they do yet, we'll cover them in detail soon. Some examples of plumbing commands are:
+
+git apply
+git commit-tree
+git hash-object
+We'll focus on the high-level commands because that's what you use 99% of the time as a developer, but we'll dip down into the low-level commands occasionally to really understand how Git works.
 
 */
