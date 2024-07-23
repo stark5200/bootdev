fa7d7bd89e2b71c8651f94db1974b3e357cabaa0
stark5200 <a.taha5200@gmail.com>
2024-07-23 11:48:41 +0300

    git cat-file

--- go/git/main.go
+++ go/git/main.go
@@ -16,4 +16,17 @@ git commit-tree
 git hash-object
 We'll focus on the high-level commands because that's what you use 99% of the time as a developer, but we'll dip down into the low-level commands occasionally to really understand how Git works.
 
+// how to view git config file 
+cat ~/.gitconfig
+
+//
+git cat-file -p 6ee62cb5ceef467b5a3f0929bc2b760f89efa4e9
+tree 5b21d4f16a4b07a6cde5a3242187f6a5a68b060f
+author stark5200 <a.taha5200@gmail.com> 1721674649 +0300
+committer stark5200 <a.taha5200@gmail.com> 1721674683 +0300
+
+A: add contents.md
+stark@Ammars-Laptop webflyx % 
+//
+
 */
