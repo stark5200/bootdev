12ec4bbf8ea0219d99bcddfc7161332e9162e169
stark5200 <a.taha5200@gmail.com>
2024-07-24 20:10:06 +0300

    new branch

--- go/git/main.go
+++ go/git/main.go
@@ -42,4 +42,20 @@ In my experience, 90% of the time you will be using --global to set things like 
 OVERRIDING
 If you set a configuration in a more specific location, it will override the same configuration in a more general location. For example, if you set user.name in the local configuration, it will override the user.name set in the global configuration.
 //
+
+//
+NEW BRANCH
+You should already be on the main branch: your "default" branch. You can always check with git branch.
+
+TWO WAYS TO CREATE A BRANCH
+git branch my_new_branch
+Copy icon
+This creates a new branch called my_new_branch. The thing is, I rarely use this command because usually I want to create a branch and switch to it immediately. So I use this command instead:
+
+git switch -c my_new_branch
+Copy icon
+The switch command allows you to switch branches, and the -c flag tells Git to create a new branch if it doesn't already exist.
+
+When you create a new branch, it uses the current commit you are on as the branch base. For example, if you're on your main branch with 3 commits, A, B, and C, and then you run git switch -c my_new_branch, your new branch will look like this:
+//
 */
