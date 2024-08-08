c2a85eb18203f341c4a9c19e3858cee10801b12a
stark5200 <a.taha5200@gmail.com>
2024-08-08 23:29:46 +0200

    interfaces 2

--- go/interfaces/main.go
+++ go/interfaces/main.go
@@ -59,3 +59,35 @@ type sendingReport struct {
 func (sr sendingReport) getMessage() string {
 	return fmt.Sprintf(`Your "%s" report is ready. You've sent %v messages.`, sr.reportName, sr.numberOfSends)
 }
+
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getSalary() int {
+	return c.hourlyPay * c.hoursPerYear
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
