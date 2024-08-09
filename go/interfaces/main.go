57cb0a01d895febcb5d721d3881309e0bd269853
stark5200 <a.taha5200@gmail.com>
2024-08-09 20:56:11 +0200

    type assertions

--- go/interfaces/main.go
+++ go/interfaces/main.go
@@ -121,3 +121,65 @@ type email struct {
 	isSubscribed bool
 	body         string
 }
+
+/*
+Type assertions 
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+c, ok := s.(circle)
+if !ok {
+	// log an error if s isn't a circle
+	log.Fatal("s is not a circle")
+}
+
+radius := c.radius
+*/
+
+func getExpenseReport(e expense) (string, float64) {
+	// ?
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
