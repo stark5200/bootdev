db4d3f2fa9fb22a3468aff75793bcd5f964208a2
stark5200 <a.taha5200@gmail.com>
2024-08-13 01:46:41 +0200

    error interface chapter

---
+++ go/errorInterface/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+/*
+Error Interface explanation
+
+type error interface {
+    Error() string
+}
+Copy icon
+When something can go wrong in a function, that function should return an error as its last return value. Any code that calls a function that can return an error should handle errors by testing whether the error is nil.
+
+// Atoi converts a stringified number to an integer
+i, err := strconv.Atoi("42b")
+if err != nil {
+    fmt.Println("couldn't convert:", err)
+    // because "42b" isn't a valid integer, we print:
+    // couldn't convert: strconv.Atoi: parsing "42b": invalid syntax
+    // Note:
+    // 'parsing "42b": invalid syntax' is returned by the .Error() method
+    return
+}
+// if we get here, then
+// i was converted successfully
+
+*/
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	// ?
+}
+
+// don't edit below this line
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
+}
