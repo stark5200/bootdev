94029a4f76eef65cdc8fae4bdc06403ca36c0026
stark5200 <a.taha5200@gmail.com>
2024-08-12 05:52:08 +0200

    interfaces c1

--- go/interfaces/main.go
+++ go/interfaces/main.go
@@ -242,3 +242,33 @@ func getExpenseReport2(e expense2) (string, float64) {
 		return "", 0.0
 	}
 }
+
+type Formatter interface {
+	Format() string
+}
+
+type PlainText struct {
+	message string
+}
+type Bold struct {
+	message string
+}
+type Code struct {
+	message string
+}
+
+func (p PlainText) Format() string {
+	return p.message
+}
+func (b Bold) Format() string {
+	return ("**"+b.message+"**")
+}
+func (c Code) Format() string {
+	return "`"+c.message+"`"
+}
+
+// Don't Touch below this line
+
+func SendMessage(formatter Formatter) string {
+	return formatter.Format() // Adjusted to call Format without an argument
+}
