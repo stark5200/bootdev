529133a15bb9a09defb1b7c0b7d95dbd4bc6a233
stark5200 <a.taha5200@gmail.com>
2024-08-17 00:02:10 +0200

    error interface 2

--- go/errorInterface/main.go
+++ go/errorInterface/main.go
@@ -97,6 +97,7 @@ import (
 	"fmt"
 )
 
-func getSMSErrorString(cost float64, recipient string) string {
-	// ?
+func getSMSErrorString(cost float64, recipient string) (s string) {
+	s = fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient)
+	return
 }
