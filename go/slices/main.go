05951a1d308f3f5dcef5cae20a4718812a214302
stark5200 <a.taha5200@gmail.com>
2024-08-24 05:16:48 +0200

    message filter

--- go/slices/main.go
+++ go/slices/main.go
@@ -306,3 +306,47 @@ for i, word := range msg {
 return -1
 }
 
+type Message interface {
+	Type() string
+}
+
+type TextMessage struct {
+	Sender  string
+	Content string
+}
+
+func (tm TextMessage) Type() string {
+	return "text"
+}
+
+type MediaMessage struct {
+	Sender    string
+	MediaType string
+	Content   string
+}
+
+func (mm MediaMessage) Type() string {
+	return "media"
+}
+
+type LinkMessage struct {
+	Sender  string
+	URL     string
+	Content string
+}
+
+func (lm LinkMessage) Type() string {
+	return "link"
+}
+
+// Don't touch above this line
+
+func filterMessages(messages []Message, filterType string) []Message {
+	filtered := []Message{}
+	for _, msg := range messages {
+		if msg.Type() == filterType {
+			filtered = append(filtered, msg)
+		}
+	}
+	return filtered
+}
