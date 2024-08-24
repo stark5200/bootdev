4add02ff090aa86773e09db2a048fcd9e6ecc271
stark5200 <a.taha5200@gmail.com>
2024-08-24 02:05:18 +0200

    append

--- go/slices/main.go
+++ go/slices/main.go
@@ -185,3 +185,67 @@ Complete the sum function to return the sum of all inputs.
 Take note of how the variadic inputs and the spread operator are used in the test suite.
 */
 
+/*
+Append
+The built-in append function is used to dynamically add elements to a slice:
+
+func append(slice []Type, elems ...Type) []Type
+Copy icon
+If the underlying array is not large enough, append() will create a new underlying array and point the slice to it.
+
+Notice that append() is variadic, the following are all valid:
+
+slice = append(slice, oneThing)
+slice = append(slice, firstThing, secondThing)
+slice = append(slice, anotherSlice...)
+Copy icon
+Assignment
+We've been asked to total costs per day, in a given period.
+
+Complete the getCostsByDay() function using the append() function. It accepts a slice of cost structs and returns a slice of float64s, where each float64 represents the total cost for a day.
+
+The length of the returned slice should be inferred from the highest numbered day field. Some days may have multiple costs, while others may have none. Include all actual and implied totals in the returned slice, starting with day '0'. Use the append() function to dynamically increase the size of the returned slice when needed.
+
+Example
+Given this input:
+
+[]cost{
+    {0, 4.0},
+    {1, 2.1},
+    {5, 2.5},
+    {1, 3.1},
+}
+Copy icon
+We expect this result:
+
+[]float64{
+    4.0, // first day
+    5.2, // 2.1 + 3.1
+    0.0, // intermediate days with no costs
+    0.0, // ...
+    0.0, // ...
+    2.5, // last day
+}
+*/
+type cost struct {
+	day   int
+	value float64
+}
+
+func getCostsByDay(costs []cost) []float64 {
+	costsPerDay := []float64{}
+	for i := 0; i < len(costs); i++ {
+		for len(costsPerDay) <= costs[i].day{
+			costsPerDay = append(costsPerDay, 0.0)
+		}
+	}
+	for j := 0; j < len(costsPerDay); j++ {
+		for k := 0; k < len(costs); k ++ {
+			if costs[k].day == j {
+			costsPerDay[j] += costs[k].value
+			}
+		}
+	} 
+	return costsPerDay
+}
+
