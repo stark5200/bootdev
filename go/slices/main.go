73ff89e0a49eb8aea93e04ec5e60c990158e9c6d
stark5200 <a.taha5200@gmail.com>
2024-08-25 23:21:13 +0200

    password strength

--- go/slices/main.go
+++ go/slices/main.go
@@ -350,3 +350,21 @@ func filterMessages(messages []Message, filterType string) []Message {
 	}
 	return filtered
 }
+
+//// Password strength
+
+/*
+Password Strength
+As part of improving security, Textio wants to enforce a new password policy. A valid password must meet the following criteria:
+
+At least 5 characters long but no more than 12 characters.
+Contains at least one uppercase letter.
+Contains at least one digit.
+Assignment
+Implement the isValidPassword function. Use a loop to inspect each character in the password string to check for its length, and the presence of an uppercase letter and a digit.
+*/
+
+func isValidPassword(password string) bool {
+	// ?
+	return false
+}
