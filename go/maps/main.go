703f425f6ce4dc624948f053c3eff107a7513bc7
stark5200 <a.taha5200@gmail.com>
2024-08-26 22:57:05 +0200

    mutations

--- go/maps/main.go
+++ go/maps/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Maps
 Maps are similar to JavaScript objects, Python dictionaries, and Ruby hashes. Maps are a data structure that provides key->value mapping.
@@ -60,4 +62,54 @@ func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
 type user struct {
 	name        string
 	phoneNumber int
+}
+
+/*
+Mutations
+Insert an element
+m[key] = elem
+Copy icon
+Get an element
+elem = m[key]
+Copy icon
+Delete an element
+delete(m, key)
+Copy icon
+Check if a key exists
+elem, ok := m[key]
+Copy icon
+If key is in m, then ok is true. If not, ok is false.
+
+If key is not in the map, then elem is the zero value for the map's element type.
+
+Assignment
+It's important to keep up with privacy regulations and to respect our user's data. We need a function that will delete user records.
+
+Complete the deleteIfNecessary function.
+
+Check the scheduledForDeletion bool to determine if they are scheduled for deletion or not.
+
+If the user doesn't exist in the map, return the error not found.
+If they exist but aren't scheduled for deletion, return deleted as false with no errors.
+If they exist and are scheduled for deletion, return deleted as true with no errors and delete their record from the map.
+Note on passing maps
+Like slices, maps are also passed by reference into functions. This means that when a map is passed into a function we write, we can make changes to the original, we don't have a copy.
+*/
+
+func deleteIfNecessary(users map[string]user2, name string) (deleted bool, err error) {
+	elem, ok := users[name]
+	if !ok {
+		return false, fmt.Errorf("not found")
+	} 
+	if !elem.scheduledForDeletion {
+		return false, nil
+	}
+	delete(users, name)
+	return true, nil
+}
+
+type user2 struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
 }
