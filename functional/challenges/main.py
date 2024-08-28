1fc25d00e69c2aa88527daaa1260c99ceb515edb
stark5200 <a.taha5200@gmail.com>
2024-08-29 01:43:47 +0200

    c2 done

--- functional/challenges/main.py
+++ functional/challenges/main.py
@@ -1,21 +1,13 @@
-# Chapter 1 Challenge 2
-
 def hex_to_rgb(hex_color):
-    if is_hexadecimal(hex_color):
-        return int(hex_color[:2], 16), int(hex_color[2:4], 16), int(hex_color[4:], 16)
-    return Exception("not a hex color string")
+    if not is_hexadecimal(hex_color) or len(hex_color) != 6:
+        raise Exception("not a hex color string")
+    
+    return int(hex_color[:2], 16), int(hex_color[2:4], 16), int(hex_color[4:], 16)
 
 
 def is_hexadecimal(hex_string):
-    a = False
-    if len(hex_string) == 6:
-        a = True
-    b = False
-    if int(hex_string, 16):
-        b = True
-
     try:
-        a and b
+        int(hex_string, 16)
         return True
-    except:
-        return Exception("not a hex color string")
+    except Exception:
+        return False
