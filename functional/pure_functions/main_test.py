cbf1025a228f8a3ec893935e9e8bd95823c06660
stark5200 <a.taha5200@gmail.com>
2024-08-28 21:57:13 +0200

    back to functional

--- functional/pure_functions/main_test.py
+++ functional/pure_functions/main_test.py
@@ -1,5 +1,13 @@
 import main as main_
 
+def get_median_font_size(font_sizes):
+    if (len(font_sizes) == 0):
+        return None
+    if len(font_sizes)%2 == 0:
+         return sorted(font_sizes)[(len(font_sizes)//2 - 1)]
+    return sorted(font_sizes)[(len(font_sizes)//2)]
+
+
 run_cases = [
     ("Proposal.docx", "pdf", "Proposal.pdf"),
     ("Invoice.txt", "md", "Invoice.md"),
