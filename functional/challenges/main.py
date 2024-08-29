f09dbfd9f4e63ced2ba19487bea1c1f7cfa58f94
stark5200 <a.taha5200@gmail.com>
2024-08-29 02:03:19 +0200

    chapter 1 functional with challenges done

--- functional/challenges/main.py
+++ functional/challenges/main.py
@@ -1,3 +1,5 @@
+# Challenge 2 chapter 1, main_test.py
+
 def hex_to_rgb(hex_color):
     if not is_hexadecimal(hex_color) or len(hex_color) != 6:
         raise Exception("not a hex color string")
@@ -11,3 +13,28 @@ def is_hexadecimal(hex_string):
         return True
     except Exception:
         return False
+      
+# Challenge 3 chapter 1, main_test1.py
+
+def deduplicate_lists(lst1, lst2, reverse=False):
+    lst3 = []
+    lst3 = add_unique(lst3, lst1)
+    lst3 = add_unique(lst3, lst2)
+    
+    return sorted(lst3, reverse = reverse)
+
+def is_empty(lst):
+    if len(lst) == 0:
+        return True
+    return False
+
+def add_unique(old_lst, new_lst):
+    if is_empty(new_lst):
+        return old_lst
+    lst = old_lst
+    for i in new_lst:
+        if i not in lst:
+            lst.append(i)
+    return lst
+    
+
