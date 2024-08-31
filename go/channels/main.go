97eef480bd1334d35592eec15b605c7e546644f0
stark5200 <a.taha5200@gmail.com>
2024-08-31 05:48:13 +0200

    buffered channel

--- go/channels/main.go
+++ go/channels/main.go
@@ -129,3 +129,28 @@ func getDBsChannel(numDBs int) (chan struct{}, *int) {
 
 	return ch, &count
 }
+
+/*
+Buffered Channels
+Channels can optionally be buffered.
+
+Creating a channel with a buffer
+You can provide a buffer length as the second argument to make() to create a buffered channel:
+
+ch := make(chan int, 100)
+Copy icon
+A buffer allows the channel to hold a fixed number of values before sending blocks. This means sending on a buffered channel only blocks when the buffer is full, and receiving blocks only when the buffer is empty.
+
+Assignment
+We want to be able to send emails in batches. A writing goroutine will write an entire batch of email messages to a buffered channel, and later, once the channel is full, a reading goroutine will read all of the messages from the channel and send them out to our clients.
+
+Complete the addEmailsToQueue function. It should create a buffered channel with a buffer large enough to store all of the emails it's given. It should then write the emails to the channel in order, and finally return the channel.
+*/
+
+func addEmailsToQueue(emails []string) chan string {
+	emailsToSend := make(chan string, len(emails))
+	for _, email := range emails {
+		emailsToSend <- email
+	}
+	return emailsToSend
+}
