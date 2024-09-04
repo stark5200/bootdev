0330d62ff40ae2929487ad420c0e705f4ed1b64c
stark5200 <a.taha5200@gmail.com>
2024-09-04 02:47:43 +0200

    SELECT

--- go/channels/main.go
+++ go/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func sendEmail(message string) {
@@ -239,7 +240,13 @@ Return the slice
 */
 
 func concurrentFib(n int) []int {
-	// ?
+	fibSlice := []int{}
+	fibs := make(chan int, n)
+	go fibonacci(n, fibs)
+	for i := range fibs {
+		fibSlice = append(fibSlice, i)
+	}
+	return fibSlice
 }
 
 // don't touch below this line
@@ -253,5 +260,127 @@ func fibonacci(n int, ch chan int) {
 	close(ch)
 }
 
+/// Select
+
+/*
+Select
+Sometimes we have a single goroutine listening to multiple channels and want to process data in the order it comes through each channel.
+
+A select statement is used to listen to multiple channels at the same time. It is similar to a switch statement but for channels.
+
+select {
+case i, ok := <- chInts:
+    fmt.Println(i)
+case s, ok := <- chStrings:
+    fmt.Println(s)
+}
+Copy icon
+The first channel with a value ready to be received will fire and its body will execute. If multiple channels are ready at the same time one is chosen randomly. The ok variable in the example above refers to whether or not the channel has been closed by the sender yet.
+
+Assignment
+Complete the logMessages function.
+
+Use an infinite for loop and a select statement to log the emails and sms messages as they come in order across the two channels. Add a condition to return from the function when one of the two channels closes, whichever is first.
+
+Use the logSms and logEmail functions to log the messages.
+*/
+
+
+
+func logMessages(chEmails, chSms chan string) {
+	for {
+		select {
+		case s, ok := <- chSms:
+			if !ok {
+				return
+			}
+			logSms(s)
+		case e, ok := <- chEmails:
+			if !ok {
+				return
+			}
+			logEmail(e)
+		}
+	}
+}
+
+// don't touch below this line
+
+func logSms(sms string) {
+	fmt.Println("SMS:", sms)
+}
+
+func logEmail(email string) {
+	fmt.Println("Email:", email)
+}
+
+func test2(sms []string, emails []string) {
+	fmt.Println("Starting...")
+
+	chSms, chEmails := sendToLogger(sms, emails)
+
+	logMessages(chEmails, chSms)
+	fmt.Println("===============================")
+}
+
+func main2() {
+	rand.Seed()
+	test2(
+		[]string{
+			"hi friend",
+			"What's going on?",
+			"Welcome to the business",
+			"I'll pay you to be my friend",
+		},
+		[]string{
+			"Will you make your appointment?",
+			"Let's be friends",
+			"What are you doing?",
+			"I can't believe you've done this.",
+		},
+	)
+	test2(
+		[]string{
+			"this song slaps hard",
+			"yooo hoooo",
+			"i'm a big fan",
+		},
+		[]string{
+			"What do you think of this song?",
+			"I hate this band",
+			"Can you believe this song?",
+		},
+	)
+}
+
+func sendToLogger(sms, emails []string) (chSms, chEmails chan string) {
+	chSms = make(chan string)
+	chEmails = make(chan string)
+	go func() {
+		for i := 0; i < len(sms) && i < len(emails); i++ {
+			done := make(chan struct{})
+			s := sms[i]
+			e := emails[i]
+			t1 := time.Millisecond * time.Duration(rand.Intn(1000))
+			t2 := time.Millisecond * time.Duration(rand.Intn(1000))
+			go func() {
+				time.Sleep(t1)
+				chSms <- s
+				done <- struct{}{}
+			}()
+			go func() {
+				time.Sleep(t2)
+				chEmails <- e
+				done <- struct{}{}
+			}()
+			<-done
+			<-done
+			time.Sleep(10 * time.Millisecond)
+		}
+		close(chSms)
+		close(chEmails)
+	}()
+	return chSms, chEmails
+}
 
 
