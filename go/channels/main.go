1de5c61a9baacbf0be67650fad0dc679e7ea0103
stark5200 <a.taha5200@gmail.com>
2024-09-04 03:08:23 +0200

    done with channels, now for challenges

--- go/channels/main.go
+++ go/channels/main.go
@@ -383,4 +383,32 @@ func sendToLogger(sms, emails []string) (chSms, chEmails chan string) {
 	return chSms, chEmails
 }
 
+func saveBackups(snapshotTicker, saveAfter <-chan time.Time, logChan chan string) {
+	for {
+		select {
+		case <-snapshotTicker:
+			takeSnapshot(logChan)
+		case <-saveAfter:
+			saveSnapshot(logChan)
+			return
+		default:
+			waitForData(logChan)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
 
+// don't touch below this line
+
+func takeSnapshot(logChan chan string) {
+	logChan <- "Taking a backup snapshot..."
+}
+
+func saveSnapshot(logChan chan string) {
+	logChan <- "All backups saved!"
+	close(logChan)
+}
+
+func waitForData(logChan chan string) {
+	logChan <- "Nothing to do, waiting..."
+}
