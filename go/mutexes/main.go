f9aaff33e32ad3dea3c5d99a73b30eedcaaff79b
stark5200 <a.taha5200@gmail.com>
2024-09-06 22:41:34 +0200

    rw mutex

--- go/mutexes/main.go
+++ go/mutexes/main.go
@@ -79,3 +79,36 @@ It's the same with generics, we just have a variable that represents the type:
 
 var myZero T
 */
+
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mu     *sync.RWMutex
+}
+
+func (sc safeCounter) inc(key string) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	sc.slowIncrement(key)
+}
+
+func (sc safeCounter) val(key string) int {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	return sc.counts[key]
+}
+
+// don't touch below this line
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
