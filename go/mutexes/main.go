741f68980b7fd1f46f31f05ee8ec7d986e711c28
stark5200 <a.taha5200@gmail.com>
2024-09-11 23:38:40 +0200

    constraints

--- go/mutexes/main.go
+++ go/mutexes/main.go
@@ -222,3 +222,55 @@ Go places an emphasis on simplicity. In other words, Go has purposefully left ou
 
 According to historical data from Go surveys, Go’s lack of generics has always been listed as one of the top three biggest issues with the language. At a certain point, the drawbacks associated with the lack of a feature like generics justify adding complexity to the language.
 */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
+	// ?
+}
+
+// don't edit below this line
+
+type lineItem interface {
+	GetCost() float64
+	GetName() string
+}
+
+type subscription struct {
+	userEmail string
+	startDate time.Time
+	interval  string
+}
+
+func (s subscription) GetName() string {
+	return fmt.Sprintf("%s subscription", s.interval)
+}
+
+func (s subscription) GetCost() float64 {
+	if s.interval == "monthly" {
+		return 25.00
+	}
+	if s.interval == "yearly" {
+		return 250.00
+	}
+	return 0.0
+}
+
+type oneTimeUsagePlan struct {
+	userEmail        string
+	numEmailsAllowed int
+}
+
+func (otup oneTimeUsagePlan) GetName() string {
+	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+}
+
+func (otup oneTimeUsagePlan) GetCost() float64 {
+	const costPerEmail = 0.03
+	return float64(otup.numEmailsAllowed) * costPerEmail
+}
