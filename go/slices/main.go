fbaf018c6068f6a60d3f851314f9e647141a1f01
stark5200 <a.taha5200@gmail.com>
2024-09-13 00:21:26 +0200

    challenge 6

--- go/slices/main.go
+++ go/slices/main.go
@@ -3,6 +3,8 @@ package main
 import (
 		"unicode"
 		"strings"
+		"errors"
+		"fmt"
 )
 
 /*
@@ -509,3 +511,47 @@ The Mailio API needs a very robust error-logging system so we can see when thing
 
 Complete the getLogger function. It should take as input a formatter function and return a new function. The new logger function takes as input two strings and passes them to the formatter, then prints the result. Keep the order of the strings.
 */
+
+// getLogger takes a function that formats two strings into
+// a single string and returns a function that formats two strings but prints
+// the result instead of returning it
+func getLogger(formatter func(string, string) string) func(string, string) {
+	return func(first, second string) {
+		fmt.Println(formatter(first, second))
+	}
+}
+
+// don't touch below this line
+
+
+func test(first string, errors []error, formatter func(string, string) string) {
+	defer fmt.Println("====================================")
+	logger := getLogger(formatter)
+	fmt.Println("Logs:")
+	for _, err := range errors {
+		logger(first, err.Error())
+	}
+}
+
+func colonDelimit(first, second string) string {
+	return first + ": " + second
+}
+func commaDelimit(first, second string) string {
+	return first + ", " + second
+}
+
+func main() {
+	dbErrors := []error{
+		errors.New("out of memory"),
+		errors.New("cpu is pegged"),
+		errors.New("networking issue"),
+		errors.New("invalid syntax"),
+	}
+	test("Error on database server", dbErrors, colonDelimit)
+
+	mailErrors := []error{
+		errors.New("email too large"),
+		errors.New("non alphanumeric symbols found"),
+	}
+	test("Error on mail server", mailErrors, commaDelimit)
+}
