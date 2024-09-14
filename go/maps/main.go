de663ff844a6b38732bc48079e995271388d3ee3
stark5200 <a.taha5200@gmail.com>
2024-09-14 20:04:03 +0200

    maps challenge 1

--- go/maps/main.go
+++ go/maps/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 Maps
@@ -195,3 +198,36 @@ func getNameCounts(names []string) map[rune]map[string]int {
 	}
 	return nameCounts
 }	
+
+/// Challenge 1
+
+/*
+Distinct Words
+Complete the countDistinctWords function using a map. It should take a slice of strings and return the total count of distinct words across all the strings. Assume words are separated by spaces. Casing should not matter. (e.g., "Hello" and "hello" should be considered the same word).
+
+For example:
+
+messages := []string{"Hello world", "hello there", "General Kenobi"}
+count := countDistinctWords(messages)
+Copy icon
+count should be 5 as the distinct words are "hello", "world", "there", "general" and "kenobi" irrespective of casing.
+
+Hint
+Go's strings package can be very helpful here. Specifically the Fields method and the ToLower method.
+*/
+
+func countDistinctWords(messages []string) int {
+	distinct := make(map[string]int)
+	for _, msg := range messages {
+		words := strings.Fields(strings.ToLower(msg))
+		for _, word := range words {
+			val, ok := distinct[word]
+			if ok {
+			    distinct[word] = val + 1
+			} else {
+				distinct[word] = 1
+			}
+		}
+	}
+	return len(distinct)
+}
