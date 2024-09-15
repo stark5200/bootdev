b3ccbafa1ffb8b88b70cd05fcebfd05898d8451c
stark5200 <a.taha5200@gmail.com>
2024-09-15 02:30:20 +0200

    c3 defer

--- go/maps/main.go
+++ go/maps/main.go
@@ -286,3 +286,39 @@ func contains(stringSlice []string, item string) bool {
 
 	return false
 }
+
+/// Challenge 3
+
+/*
+Log and Delete
+Assignment
+There is a bug in the logAndDelete function, fix it!
+
+This function should always delete the user from the user's map, which is a map that stores the user's name as keys. It also returns a log string that indicates to the caller some information about the user's deletion.
+
+To avoid bugs like this in the future, instead of calling delete before each return, just defer the delete once at the beginning of the function.
+*/
+
+const (
+	logDeleted  = "user deleted"
+	logNotFound = "user not found"
+	logAdmin    = "admin deleted"
+)
+
+type c3user struct {
+	name   string
+	number int
+	admin  bool
+}
+
+func logAndDelete(users map[string]c3user, name string) (log string) {
+	defer delete(users, name)
+	c3user, ok := users[name]
+	if !ok {
+		return logNotFound
+	}
+	if c3user.admin {
+		return logAdmin
+	}
+	return logDeleted
+}
