e1e50d9848632476cda1f76eab4af3c4a85d0cd9
stark5200 <a.taha5200@gmail.com>
2024-09-18 02:55:54 +0200

    http go 2

--- http_clients/http.go
+++ http_clients/http.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func getItemData() ([]byte, error) {
-	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/items")
+func getItemData(url string) ([]byte, error) {
+	res, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
 	defer res.Body.Close()
 
 	data, err := io.ReadAll(res.Body)
-	return data, err
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+
+	return data, nil
+}
+
+func prettify(data string) (string, error) {
+	var prettyJSON bytes.Buffer
+	err := json.Indent(&prettyJSON, []byte(data), "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("error indenting JSON: %w", err)
+	}
+	return prettyJSON.String(), nil
 }
