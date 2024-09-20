d86c775bccdbc3722e36b36789ccd6c84deef7d1
stark5200 <a.taha5200@gmail.com>
2024-09-20 22:45:07 +0200

    net http

--- http_clients/http.go
+++ http_clients/http.go
@@ -48,3 +48,41 @@ func getItemData(url string) ([]byte, error) {
 	defer res.Body.Close()
 	return res, nil
 }
+
+/*
+net/http
+In this course, we'll be using Go's standard net/http package and the http.Client to make HTTP requests. In fact, we've already been using it! The http.Get function uses the http.DefaultClient under the hood.
+
+Making a Request
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getSpells() ([]byte, error) {
+	res, err := http.Get("https://api.fantasyquest.com/spell")
+	if err != nil {
+		return []byte{}, fmt.Errorf("error making request: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error reading response: %w", err)
+	}
+	return data, nil
+}
+Copy icon
+We'll go in-depth on the various things happening here later, but let's cover some basics for now.
+
+http.Get uses the http.DefaultClient to make a request to the given url
+res is the HTTP response that comes back from the server
+defer res.Body.Close() ensures that the response body is properly closed after reading. Not doing so can cause memory issues.
+io.ReadAll reads the response body into a slice of bytes []byte called data
+Assignment
+There is a bug in the getItemData function! It's returning the entire http.Response instead of the data from the body (a slice of bytes). Fix it so that it returns []byte.
+
+Use io.ReadAll to read the .Body of the response.
+Return the resulting []byte
+*/
