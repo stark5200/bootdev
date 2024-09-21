ee7c1aaa37b1105df5f148aa257d1811004aaf62
stark5200 <a.taha5200@gmail.com>
2024-09-21 23:53:55 +0200

    main go 2

--- http_clients/main.go
+++ http_clients/main.go
@@ -18,3 +18,37 @@ func main() {
 	}
 	fmt.Println(prettyData)
 }
+
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+const itemURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/items"
+
+func main() {
+	items, err := getItemData(itemURL)
+	if err != nil {
+		log.Fatalf("error getting item data: %v", err)
+	}
+	// indent properly and print
+	prettyData, err := prettify(string(items))
+	if err != nil {
+		log.Fatalf("error prettifying data: %v", err)
+	}
+	fmt.Println(prettyData)
+}
+
+func prettify(data string) (string, error) {
+	var prettyJSON bytes.Buffer
+	err := json.Indent(&prettyJSON, []byte(data), "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("error indenting JSON: %w", err)
+	}
+	return prettyJSON.String(), nil
+}
