469cd8d6cc71c428f7d3921f56a7f9d1bae99f26
stark5200 <a.taha5200@gmail.com>
2024-09-25 20:46:32 +0200

    dns quiz

--- http_clients/main.go
+++ http_clients/main.go
@@ -57,3 +57,17 @@ const playerObject = `
 	"power": 9000
 }
 `
+/*
+Web Addresses Quiz
+To recap, a domain name is part of a URL. It's the part that tells the computer where the server is located on the internet by being converted into a numerical IP address.
+
+We'll cover exactly how an IP address is used by your computer to find a path to the server in a later course. For now, it's just important to understand that an IP address is what your computer is using at a lower level to communicate on a network.
+
+Deploying a real website to the internet is actually quite simple. It involves only a couple of steps:
+
+Create a server that hosts your website files and connect it to the internet
+Acquire a domain name
+Connect the domain name to the IP address of your server
+Your server is accessible via the internet!
+
+*/
