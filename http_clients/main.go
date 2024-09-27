023756fcdc0cfd8a6e8f43d5507eab873233b4d9
stark5200 <a.taha5200@gmail.com>
2024-09-27 21:33:07 +0200

    back to dns

--- http_clients/main.go
+++ http_clients/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"io"
 	"bytes"
 	"encoding/json"
+	"net/http/"
+	"net/url"
 )
 
 const itemURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/items"
@@ -95,3 +98,45 @@ Copy icon
  Assignment
 Complete the getDomainNameFromURL function. Given a full URL, it should return the domain (or host) name. Simply return any potential errors.
 */
+
+func getIPAddress2(domain string) (string, error) {
+	url := fmt.Sprintf("https://cloudflare-dns.com/dns-query?name=%s&type=A", domain)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("accept", "application/dns-json")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error sending request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var dnsRes DNSResponse
+	if err := json.Unmarshal(body, &dnsRes); err != nil {
+		return "", fmt.Errorf("error unmarshalling json: %w", err)
+	}
+
+	if len(dnsRes.Answer) == 0 {
+		return "", fmt.Errorf("no answer found")
+	}
+
+	return dnsRes.Answer[0].Data, nil
+}
+
+func getDomainNameFromURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		fmt.Println("error parsing url:", err)
+		return "", err
+	}
+	return parsedURL.Hostname(), err
+}
