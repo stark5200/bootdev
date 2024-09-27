91a6e8e0ea252ed56c208cadd6c28439ba710bea
stark5200 <a.taha5200@gmail.com>
2024-09-28 00:51:11 +0200

    pragma and struct

--- memory_management/main.c
+++ memory_management/main.c
@@ -54,3 +54,51 @@ int main() {
 
 
 }
+
+/*
+Pragma Once and Header Guards
+We saw how .h header files are used in a previous lesson, but before we go further let's talk about a potential issue you might run into: multiple inclusions. If the same header file gets included more than once, you can end up with some nasty errors caused by redefining things like functions or structs.
+
+ Pragma once
+One simple solution (and the one we'll use for the rest of this course) is #pragma once. Adding this line to the top of a header file tells the compiler to include the file only once, even if it's referenced multiple times across your program.
+
+// my_header.h
+
+#pragma once
+
+struct Point {
+    int x;
+    int y;
+};
+Copy icon
+ Header Guards
+Another common way to avoid multiple inclusions is with include guards, which use preprocessor directives like this:
+
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+// some cool code
+
+#endif
+Copy icon
+This method works by defining a unique macro for the header file. If it’s already been included, the guard prevents it from being processed again.
+
+Throughout this course, you’ll see #pragma once in our header files. It's quicker and less error-prone than traditional include guards, and it works well with most modern compilers.
+ */
+
+struct Coordinate {
+  int x;
+  int y;
+  int z;
+};
+
+struct Coordinate new_coord(int x, int y, int z) {
+  struct Coordinate c = {
+  .x = x, 
+  .y = y,
+  .z = z,
+  };
+
+  return c;
+}
+
