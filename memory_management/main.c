ddb2cbe01c35f6a05cb80dc15ef01df273f56354
stark5200 <a.taha5200@gmail.com>
2024-09-29 23:49:20 +0200

    some tests

--- memory_management/main.c
+++ memory_management/main.c
@@ -99,6 +99,21 @@ struct Coordinate new_coord(int x, int y, int z) {
   .z = z,
   };
 
+  // or type def
+
+  /*
+    #pragma once
+
+    typedef struct Coordinate {
+    int x;
+    int y;
+    int z;
+    } coordinate_t;
+
+    coordinate_t new_coord(int x, int y, int z);
+    coordinate_t scale_coordinate(coordinate_t coord, int factor);
+   */
+
   return c;
 }
 
