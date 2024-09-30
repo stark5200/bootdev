c209fc3e73a16a66d07bfca5aa11920b65e38cff
stark5200 <a.taha5200@gmail.com>
2024-09-30 04:27:05 +0200

    c strings

--- memory_management/exercise.c
+++ memory_management/exercise.c
@@ -3,3 +3,17 @@
 float get_average(int x, int y, int z) {
   return (float)(x + y + z)/3; 
 }
+
+void concat_strings(char *str1, const char *str2) {
+  int i = 0;
+  int j = 0;
+  while (*(str1 + i) != '\0') {
+    i++;
+  };
+  while (*(str2 + j) != '\0') {
+    *(str1 + i) = *(str2 + j);
+    j++;
+    i++;
+  };
+  *(str1 + i) = '\0';
+}
--- memory_management/exercise.h
+++ memory_management/exercise.h
@@ -1,1 +1,2 @@
 float get_average(int x, int y, int z);
+void concat_strings(char *str1, const char *str2);
