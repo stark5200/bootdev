0ebad3674d37400c522c87f22c728926682ebffa
stark5200 <a.taha5200@gmail.com>
2024-10-02 19:46:03 +0200

    snrk

--- memory_management/exercise.c
+++ memory_management/exercise.c
@@ -17,3 +17,32 @@ void concat_strings(char *str1, const char *str2) {
   };
   *(str1 + i) = '\0';
 }
+
+
+/*
+#include <stdio.h>
+
+#include "exercise.h"
+
+void format_object(snek_object_t obj, char *buffer) {
+  // ?
+}
+
+// don't touch below this line'
+
+snek_object_t new_integer(int i) {
+  return (snek_object_t){
+    .kind = INTEGER,
+    .data = {.v_int = i}
+  };
+}
+
+snek_object_t new_string(char *str) {
+  // NOTE: We will learn how to copy this data later.
+  return (snek_object_t){
+    .kind = STRING,
+    .data = {.v_string = str}
+  };
+}
+
+ */
