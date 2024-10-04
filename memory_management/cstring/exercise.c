cd90da3f6309fd4026698ea56817adb7f5ed3699
stark5200 <a.taha5200@gmail.com>
2024-10-04 21:44:03 +0200

    memory layout

--- memory_management/cstring/exercise.c
+++ memory_management/cstring/exercise.c
@@ -17,3 +17,25 @@ int smart_append(TextBuffer* dest, const char* src) {
   dest->length += src_len;
   return 1;
 }
+
+
+typedef union {
+  int value;
+  unsigned int err;
+} val_or_err_t;
+
+int main() {
+  val_or_err_t lanes_score = {
+    .value = -420
+  };
+  printf("value (set): %d\n", lanes_score.value);
+  printf("err (unset): %u\n", lanes_score.err);
+
+  
+  val_or_err_t teejs_score = {
+    .err = UINT_MAX
+  };
+  printf("value (unset): %d\n", teejs_score.value);
+  printf("err (set): %u\n", teejs_score.err);
+  
+}
