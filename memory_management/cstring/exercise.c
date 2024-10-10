4ef80fcef4c127e74ccd98d237c63e02ee5e624f
stark5200 <a.taha5200@gmail.com>
2024-10-10 21:49:56 +0200

    error handling

--- memory_management/cstring/exercise.c
+++ memory_management/cstring/exercise.c
@@ -38,3 +38,17 @@ int main() {
   printf("err (set): %u\n", teejs_score.err);
   
 }
+
+/*
+
+def get_player_record(player_id):
+    if player_id == 1:
+        return {"name": "Slayer", "level": 128}
+    if player_id == 2:
+        return {"name": "Dorgoth", "level": 300}
+    if player_id == 3:
+        return {"name": "Saruman", "level": 4000}
+    pass
+
+
+*/
