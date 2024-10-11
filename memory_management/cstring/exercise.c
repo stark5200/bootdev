64250adb2e51cd57c0b39e3cdaeea7ec917ace75
stark5200 <a.taha5200@gmail.com>
2024-10-11 23:33:26 +0200

    python error info

--- memory_management/cstring/exercise.c
+++ memory_management/cstring/exercise.c
@@ -52,3 +52,13 @@ def get_player_record(player_id):
 
 
 */
+
+/* some python error things
+
+Raising exceptions review
+Software applications aren't perfect, and user input and network connectivity are far from predictable. Despite intensive debugging and unit testing, applications will still have failure cases.
+
+Loss of network connectivity, missing database rows, out of memory issues, and unexpected user inputs can all prevent an application from performing "normally". It is your job to catch and handle any and all exceptions gracefully so that your app keeps working. When you are able to detect that something is amiss, you should be raising the errors yourself, in addition to the "default" exceptions that the Python interpreter will raise.
+
+raise Exception("something bad happened")
+*/
