0beefb1fabbf4dd2473adcd2352275c66e08337c
stark5200 <a.taha5200@gmail.com>
2024-10-14 23:10:53 +0200

    pop stack

--- memory_management/exercise.c
+++ memory_management/exercise.c
@@ -121,3 +121,46 @@ stack_t *stack_new(size_t capacity) {
 
   return stack;
 }
+
+#include "snekstack.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+void *stack_pop(stack_t *stack) {
+  // ?
+}
+
+// don't touch below this line'
+
+void stack_push(stack_t *stack, void *obj) {
+  if (stack->count == stack->capacity) {
+    stack->capacity *= 2;
+    void **temp = realloc(stack->data, stack->capacity * sizeof(void *));
+    if (temp == NULL) {
+      free(stack->data);
+      exit(1);
+    }
+    stack->data = temp;
+  }
+  stack->data[stack->count] = obj;
+  stack->count++;
+  return;
+}
+
+stack_t *stack_new(size_t capacity) {
+  stack_t *stack = malloc(sizeof(stack_t));
+  if (stack == NULL) {
+    return NULL;
+  }
+
+  stack->count = 0;
+  stack->capacity = capacity;
+  stack->data = malloc(stack->capacity * sizeof(void *));
+  if (stack->data == NULL) {
+    free(stack);
+    return NULL;
+  }
+
+  return stack;
+}
