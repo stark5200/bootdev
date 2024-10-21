72bc41d7c70d8864fa3914376b1af29f46fe65c6
stark5200 <a.taha5200@gmail.com>
2024-10-21 23:16:32 +0200

    more c objects

--- memory_management/exercise.h
+++ memory_management/exercise.h
@@ -38,3 +38,15 @@ snek_object_t new_integer(int);
 snek_object_t new_string(char *str);
 void format_object(snek_object_t obj, char *buffer);
 
+typedef enum SnekObjectKind {
+  INTEGER
+} snek_object_kind_t;
+
+typedef union SnekObjectData {
+  int v_int;
+} snek_object_data_t;
+
+typedef struct SnekObject {
+  snek_object_kind_t kind;
+  snek_object_data_t data;
+} snek_object_t;
