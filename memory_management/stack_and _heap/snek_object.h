6e2b7e021bf340dc657f4615cc6a9b80c9ec8299
stark5200 <a.taha5200@gmail.com>
2024-10-26 20:34:19 +0200

    snek multi object

--- memory_management/stack_and _heap/snek_object.h
+++ memory_management/stack_and _heap/snek_object.h
@@ -12,3 +12,22 @@ typedef struct SnekObject {
 } snek_object_t;
 
 snek_object_t *new_snek_integer(int value);
+
+#include "snekstack.h"
+#include "stdlib.h"
+#include <string.h>
+
+void stack_push_multiple_types(stack_t *s) {
+  float *pi = malloc(sizeof(float));
+  if (pi != NULL) {
+    *pi = 3.14;
+  }
+  char *snekchar = malloc(strlen("Sneklang is blazingly slow!") + 1);
+  if (snekchar != NULL) {
+    strcpy(snekchar, "Sneklang is blazingly slow!");
+  }
+  
+  stack_push(s, pi);
+  stack_push(s, snekchar);
+  
+}
