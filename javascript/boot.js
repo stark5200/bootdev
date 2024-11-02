8332a71d30e1b749d289314ff9f69ad46e1a3131
stark5200 <a.taha5200@gmail.com>
2024-11-02 22:57:48 +0100

    js 5

--- javascript/boot.js
+++ javascript/boot.js
@@ -30,3 +30,23 @@ test([ 'That', 'was', 'a', 'pleasure' ])
 test([ 'shoot!', 'I', 'cant', 'say', 'I', 'liked', 'the', 'dang', 'thing' ])
 test([ 'shoot', 'dang', 'heck' ])
 
+const movies = [
+  'oh brother where art thou',
+  'oceans eleven',
+  'fight club',
+  'the island',
+  'shutter island',
+  'the magnificent seven'
+]
+
+function logArray(arr) {
+  for (const a of arr) {
+    console.log(` - ${a}`)
+  }
+  console.log('---')
+}
+
+// don't touch above this line
+
+logArray(movies.slice(2))
+logArray(movies.slice(0, -2))
