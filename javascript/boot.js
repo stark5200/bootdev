c41b5cd87c0190e024afc9f1e338cdef5f85467a
stark5200 <a.taha5200@gmail.com>
2024-11-08 00:18:37 +0100

    movie exists

--- javascript/boot.js
+++ javascript/boot.js
@@ -67,9 +67,14 @@ printCleanReviews([ 'The movie sucks', 'I love it', 'What darn crap', 'darn, so 
 console.log('---')
 
 const movieExists = (movies, title) => {
-  // ?
+  for (let i = 0; i < movies.length; i++) {
+    console.log("Looking at:", movies[i]);
+    if (movies[i] == title) {
+      console.log("Found:", movies[i]);
+      break;
+    }
+  }
 }
-
 // don't touch below this line
 
 movieExists([ 'Incredibles', 'Tangled', 'Frozen' ], 'Frozen')
