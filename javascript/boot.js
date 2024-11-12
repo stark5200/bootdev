2dfd2878b8c3783d91007414716921f49fbe937e
stark5200 <a.taha5200@gmail.com>
2024-11-12 20:31:50 +0100

    js objects

--- javascript/boot.js
+++ javascript/boot.js
@@ -123,3 +123,24 @@ printCleanReviews2([ 'The movie sucks', 'I love it', 'What garbage', 'so sucky' 
 console.log('---')
 printCleanReviews2([ 'The movie sucks', 'I love it', 'What darn crap', 'darn, so sucky' ], 'darn')
 console.log('---')
+function getMovieRecord(title, stars, username) {
+  return {
+    title: title, 
+    stars: stars, 
+    username: username, 
+    id: title+"-"+username,
+  }
+}
+
+// Don't touch below this line
+
+logObject(getMovieRecord('oh brother where art thou', 3, 'wagslane'))
+logObject(getMovieRecord('frozen', 5.5, 'elonmusk'))
+logObject(getMovieRecord('toy story', 4, 'prince'))
+
+function logObject(obj) {
+  for (const key in obj) {
+    console.log(` - ${key}: ${obj[key]}`)
+  }
+  console.log('---')
+}
