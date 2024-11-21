cebef8633ef750046d0a4c23b81c743a4782f013
stark5200 <a.taha5200@gmail.com>
2024-11-21 23:07:49 +0100

    fat arrow

--- javascript/boot.js
+++ javascript/boot.js
@@ -206,3 +206,14 @@ const tree2 = {
     this.height /= 2
   }
 }
+const author = {
+  firstName: 'Lane',
+  lastName: 'Wagner',
+  getName: () => {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+console.log(author.getName())
+// Prints: undefined undefined
+// because the parent scope (the scope outside of the author object)
+// never defined .firstName and .lastName properties
