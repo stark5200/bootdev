172b85a1af139df071b42306dd8c657bc69f70a1
stark5200 <a.taha5200@gmail.com>
2024-11-25 23:51:41 +0100

    js errors

--- javascript/boot.js
+++ javascript/boot.js
@@ -239,3 +239,23 @@ try {
 }
 
 throw new Error('something bad happened')
+
+function getMovieRecord(movieId) {
+  if (movieId === 1) {
+    return { name: 'Apollo 13', duration: 128 }
+  }
+  if (movieId === 2) {
+    return { name: '2001: A Space Odyssey', duration: 300 }
+  }
+  if (movieId === 3) {
+    return { name: 'Interstellar', duration: 4000 }
+  }
+  throw new Error('movie id not found')
+}
+
+function logObject(obj) {
+  for (const key in obj) {
+    console.log(` - ${key}: ${obj[key]}`)
+  }
+  console.log('---')
+}
