22b9d751b6d1045490c1729f15869b26480a07c8
stark5200 <a.taha5200@gmail.com>
2024-11-30 22:11:38 +0100

    object method

--- javascript/boot.js
+++ javascript/boot.js
@@ -295,3 +295,16 @@ const review = {
 // don't touch above this line
 
 console.log(review.author.firstName)
+
+const user = {
+  getFirstReview() {
+    return this.reviews[0]
+  },
+  reviews: [ 'I hate Ice Age', 'I didn\'t enjoy it at all', 'What a fabulous film' ],
+  name: 'Bob Doogle'
+}
+
+// don't touch below this line
+
+console.log(`${user.name}'s first review is: ${user.getFirstReview()}`)
+
