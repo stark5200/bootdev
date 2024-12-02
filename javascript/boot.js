7ce2bbef76069f8137734995ae8c5f56c51925ba
stark5200 <a.taha5200@gmail.com>
2024-12-02 23:57:58 +0100

    url

--- javascript/boot.js
+++ javascript/boot.js
@@ -397,3 +397,30 @@ function getMovieRecord(movieId) {
   }
   throw new Error('movie id not found')
 }
+
+/*
+package main
+
+import (
+	"net/url"
+)
+
+func newParsedURL(urlString string) ParsedURL {
+	parsedUrl, err := url.Parse(urlString)
+	if err != nil {
+		return ParsedURL{}
+	}
+
+	return ParsedURL{
+		protocol: "",
+		username: "",
+		password: "",
+		hostname: "",
+		port:     "",
+		pathname: "",
+		search:   "",
+		hash:     "",
+	}
+}
+
+*/
