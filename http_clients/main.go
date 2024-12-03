b332c4547a46481e93d66aa79e4677df593f179a
stark5200 <a.taha5200@gmail.com>
2024-12-03 20:32:09 +0100

    http client headers

--- http_clients/main.go
+++ http_clients/main.go
@@ -140,3 +140,33 @@ func getDomainNameFromURL(rawURL string) (string, error) {
 	}
 	return parsedURL.Hostname(), err
 }
+
+// creating a new request
+req, err := http.NewRequest("GET", "https://api.example.com/users", nil)
+if err != nil {
+	fmt.Println("error creating request: ", err)
+	return
+}
+
+// setting a header on the new request
+req.Header.Set("x-api-key", "123456789")
+
+// making the request
+client := http.Client{}
+res, err := client.Do(req)
+if err != nil {
+	fmt.Println("error making request: ", err)
+	return
+}
+defer res.Body.Close()
+
+// reading a header from the response
+header := res.Header.Get("last-modified")
+fmt.Println("last modified: ", header)
+
+// deleting a header from the response
+res.Header.Del("last-modified")
+
+func getContentType(res *http.Response) string {
+	return res.Header.Get("Content-Type")
+}
