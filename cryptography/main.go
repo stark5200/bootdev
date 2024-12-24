aa5cf9d58afe31871f42990ba8551ff17db25839
stark5200 <a.taha5200@gmail.com>
2024-12-25 00:26:45 +0100

    sBox

--- cryptography/main.go
+++ cryptography/main.go
@@ -11,7 +11,8 @@ import (
   "errors"
   "math"
 	"fmt"
-	"math/rand"
+	//"math/rand"
+  "crypto/rand"
 	"log"
 )
 
@@ -312,5 +313,42 @@ func generateIV(length int) ([]byte, error) {
 		fmt.Println("Error:", err)
 	}
 	return iv, err
+}
+
+func sBox(b byte) (byte, error) {
+	//bits := make([]int, 4)
+	//for i := range 4 {
+	//	bits[i] = (b >> (3 - i)) & 1
+	//}
+	
+	var lookupTable = map[byte]byte {
+	    0b0000: 0b00,
+	    0b0001: 0b10,
+	    0b0010: 0b01,
+	    0b0011: 0b11,
+			
+		0b0100: 0b10,
+	    0b0101: 0b00,
+	    0b0110: 0b11,
+	    0b0111: 0b01,
+			
+		0b1000: 0b01,
+	    0b1001: 0b11,
+	    0b1010: 0b00,
+	    0b1011: 0b10,
+	
+		0b1100: 0b11,
+	    0b1101: 0b01,
+	    0b1110: 0b10,
+	    0b1111: 0b00,
+	}
+
+	output, ok := lookupTable[b]
+	
+	if !ok {
+		return 0, errors.New("invalid input")
+	}
+
+	return output, nil
 }
 
