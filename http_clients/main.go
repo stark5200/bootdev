4481448a2d0ffe08ae177418ca5d509c02d50f85
stark5200 <a.taha5200@gmail.com>
2024-12-29 23:07:23 +0100

    get users

--- http_clients/main.go
+++ http_clients/main.go
@@ -170,3 +170,22 @@ res.Header.Del("last-modified")
 func getContentType(res *http.Response) string {
 	return res.Header.Get("Content-Type")
 }
+
+
+func getUsers(url string) ([]User, error) {
+	fullURL := url + "?sort=experience"
+	res, err := http.Get(fullURL)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var users []User
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&users)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
