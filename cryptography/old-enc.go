44ed763035f99318b80049f37309be688aa9943a
stark5200 <a.taha5200@gmail.com>
2025-01-06 09:11:41 +0100

    noctua junk

---
+++ cryptography/old-enc.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	//"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	//"encoding/gob"
+	//"encoding/base64"
+	//"io"
+	"log"
+	//"errors"
+	//"golang.org/x/crypto/nacl/secretbox"
+)
+
+// possible considaration of embedded DB for key,value sttore vs simple .json file (redis, pebble, etc)
+
+/*
+const (
+	KeySize = 32
+	NonceSize = 24
+)
+
+type EncryptPayload struct {
+	Key *[KeySize]byte
+	Nonce *[NonceSize]byte
+	Ciphertext []byte
+}
+*/
+/*
+func generateKey() (*[KeySize]byte, error) {
+	key := new([KeySize]byte)
+	_, err := io.ReadFull(rand.Reader, key[:])
+	if err != nil {
+		log.Fatal("Error: unable to generate key, failed to read random.")
+		return nil, err
+	}
+	return key, nil
+}
+	*/
+/*
+func generateNonce() (*[NonceSize]byte, error) {
+	nonce := new([NonceSize]byte)
+	_, err := io.ReadFull(rand.Reader, nonce[:])
+	if err != nil {
+		log.Fatal("Error: unable to generate nonce, failed to read random.")
+		return nil, err
+	}
+	return nonce, nil
+}
+*/
+
+func genRsaKeys() (pubKey *rsa.PublicKey, privKey *rsa.PrivateKey, err error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &privateKey.PublicKey, privateKey, nil
+}
+
+//encryption using secretbox, uses symmetric key and nonce
+/*
+func encryptPassword(password []byte) ([]byte, *[KeySize]byte, *[NonceSize]byte, error) {
+	nonce, err := generateNonce()
+	if err != nil {
+		log.Fatal("Error: unable to generate nonce.")
+		return nil, nil, nil, err
+	}
+	key, err := generateKey()
+	if err != nil {
+		log.Fatal("Error: unable to generate key.")
+		return nil, nil, nil, err
+	}
+
+	ciphertext := make([]byte, len(nonce))
+	copy(ciphertext, nonce[:])
+	ciphertext = secretbox.Seal(ciphertext, password, nonce, key)
+	return ciphertext, key, nonce, nil
+}
+	*/
+
+//encryption using public key
+func encryptWithPublicKey(publicKey *rsa.PublicKey , text []byte) ([]byte, error) {
+	encryptedBytes, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, text, nil)
+	if err != nil {
+		log.Fatal("Error: unable to encrypt with Rsa.")
+		return nil, err
+	}
+
+	return encryptedBytes, nil
+}
+
+// decrypt password using private key
+func decryptWithPrivateKey(privKey *rsa.PrivateKey, encoded []byte) ([]byte, error) {
+	// get raw data from encoded
+	
+	decryptedBytes, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, encoded, nil)
+	if err != nil {
+		log.Fatal("Error: unable to decrypt data.")
+		return nil, err
+	}
+	return decryptedBytes, nil
+}
+
+/*
+func decryptBox(text bytes.Buffer) ([]byte, error) {
+	gobs := gob.NewDecoder(&text)
+	var encPayLoad EncryptPayload
+	err := gobs.Decode(&encPayLoad)
+	if err != nil {
+		log.Fatal("Error: unable to decode payload.")
+		return nil, err
+	}
+
+	var nonce [NonceSize]byte 
+	copy(nonce[:], encPayLoad.Ciphertext[:NonceSize])
+	
+	out, ok := secretbox.Open(nil, encPayLoad.Ciphertext[:NonceSize], encPayLoad.Nonce, encPayLoad.Key)
+	if !ok {
+		return nil, errors.New("unable to process secret box")
+	}
+
+	return out, nil
+}
+	*/
+
+func main() {
+	examplePassword := "password123"
+	fmt.Printf("password:\t %v \n", examplePassword)
+
+	// assymetric rsa encryption (encryption round 2)
+	pubKey, privKey, err := genRsaKeys()
+	if err != nil {
+		log.Fatal((err))
+	}
+	fmt.Printf("pubkey:\t %v \n privatekey:\t %v \n", pubKey, privKey)
+
+	encoded, err := encryptWithPublicKey(pubKey, []byte(examplePassword))
+	if err != nil {
+		log.Fatal((err))
+	}
+	fmt.Printf("encoded password:\t %v \n", encoded)
+	// save format for db or json
+	// db.Set([]byte(key), []byte(base64.StdEncoding.EncodeToString(encoded)))
+	//fmt.Printf("base 64 encoded password:\t %v \n", []byte(base64.StdEncoding.EncodeToString(encoded)))
+
+	//for decryption now all steps in reverse:
+	unwrapped, err := decryptWithPrivateKey(privKey, encoded)
+	if err != nil {
+		log.Fatal((err))
+	}
+	/*
+	var unwrap bytes.Buffer
+	unwrap.WriteString(string(unwrapped))
+	decrypted, err := decryptBox(unwrap)
+	if err != nil {
+		log.Fatal((err))
+	}
+	*/
+	fmt.Printf("here is your original password:\t %v \n", string(unwrapped))
+
+}
+
--- noctua/main.go
+++ noctua/main.go
@@ -1,55 +1,23 @@
 package main
 
 import (
-	"fmt"
-	//"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	//"encoding/gob"
-	//"encoding/base64"
-	//"io"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"os"
 	"log"
-	//"errors"
-	//"golang.org/x/crypto/nacl/secretbox"
 )
 
-// possible considaration of embedded DB for key,value sttore vs simple .json file (redis, pebble, etc)
+type User struct {
+	Password string `json:"password"`
+	EncryptedPassword string `json:"encryptedPassword"`
+	PrivateKey string `json:"privateKey"`
+}
 
-/*
-const (
-	KeySize = 32
-	NonceSize = 24
-)
-
-type EncryptPayload struct {
-	Key *[KeySize]byte
-	Nonce *[NonceSize]byte
-	Ciphertext []byte
-}
-*/
-/*
-func generateKey() (*[KeySize]byte, error) {
-	key := new([KeySize]byte)
-	_, err := io.ReadFull(rand.Reader, key[:])
-	if err != nil {
-		log.Fatal("Error: unable to generate key, failed to read random.")
-		return nil, err
-	}
-	return key, nil
-}
-	*/
-/*
-func generateNonce() (*[NonceSize]byte, error) {
-	nonce := new([NonceSize]byte)
-	_, err := io.ReadFull(rand.Reader, nonce[:])
-	if err != nil {
-		log.Fatal("Error: unable to generate nonce, failed to read random.")
-		return nil, err
-	}
-	return nonce, nil
-}
-*/
 
 func genRsaKeys() (pubKey *rsa.PublicKey, privKey *rsa.PrivateKey, err error) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
@@ -58,27 +26,6 @@ func genRsaKeys() (pubKey *rsa.PublicKey, privKey *rsa.PrivateKey, err error) {
 	}
 	return &privateKey.PublicKey, privateKey, nil
 }
-
-//encryption using secretbox, uses symmetric key and nonce
-/*
-func encryptPassword(password []byte) ([]byte, *[KeySize]byte, *[NonceSize]byte, error) {
-	nonce, err := generateNonce()
-	if err != nil {
-		log.Fatal("Error: unable to generate nonce.")
-		return nil, nil, nil, err
-	}
-	key, err := generateKey()
-	if err != nil {
-		log.Fatal("Error: unable to generate key.")
-		return nil, nil, nil, err
-	}
-
-	ciphertext := make([]byte, len(nonce))
-	copy(ciphertext, nonce[:])
-	ciphertext = secretbox.Seal(ciphertext, password, nonce, key)
-	return ciphertext, key, nonce, nil
-}
-	*/
 
 //encryption using public key
 func encryptWithPublicKey(publicKey *rsa.PublicKey , text []byte) ([]byte, error) {
@@ -103,62 +50,132 @@ func decryptWithPrivateKey(privKey *rsa.PrivateKey, encoded []byte) ([]byte, err
 	return decryptedBytes, nil
 }
 
-/*
-func decryptBox(text bytes.Buffer) ([]byte, error) {
-	gobs := gob.NewDecoder(&text)
-	var encPayLoad EncryptPayload
-	err := gobs.Decode(&encPayLoad)
-	if err != nil {
-		log.Fatal("Error: unable to decode payload.")
-		return nil, err
+// PrivateKeyToString converts an RSA private key to a PEM string
+func PrivateKeyToString(privateKey *rsa.PrivateKey) (string, error) {
+	// Convert the private key to DER format
+	der := x509.MarshalPKCS1PrivateKey(privateKey)
+
+	// Create a PEM block
+	pemBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: der,
 	}
 
-	var nonce [NonceSize]byte 
-	copy(nonce[:], encPayLoad.Ciphertext[:NonceSize])
-	
-	out, ok := secretbox.Open(nil, encPayLoad.Ciphertext[:NonceSize], encPayLoad.Nonce, encPayLoad.Key)
-	if !ok {
-		return nil, errors.New("unable to process secret box")
-	}
-
-	return out, nil
+	// Encode the PEM block as a string
+	return string(pem.EncodeToMemory(pemBlock)), nil
 }
-	*/
 
-func main() {
-	examplePassword := "password123"
-	fmt.Printf("password:\t %v \n", examplePassword)
+// StringToPrivateKey converts a PEM string back into an RSA private key
+func StringToPrivateKey(pemString string) (*rsa.PrivateKey, error) {
+	// Decode the PEM block
+	block, _ := pem.Decode([]byte(pemString))
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return nil, fmt.Errorf("failed to decode PEM block containing private key")
+	}
 
-	// assymetric rsa encryption (encryption round 2)
-	pubKey, privKey, err := genRsaKeys()
+	// Parse the DER-encoded key
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		log.Fatal((err))
+		return nil, fmt.Errorf("failed to parse RSA private key: %v", err)
+	}
+
+	return privateKey, nil
+}
+
+
+func encrypt(normalText string) (encrypted string, pvKey *rsa.PrivateKey, err error)  {
+	pubKey, privKey, genErr := genRsaKeys()
+	if genErr != nil {
+		log.Fatal((genErr))
+		return "", nil, genErr
 	}
 	fmt.Printf("pubkey:\t %v \n privatekey:\t %v \n", pubKey, privKey)
 
-	encoded, err := encryptWithPublicKey(pubKey, []byte(examplePassword))
+	encoded, enErr := encryptWithPublicKey(pubKey, []byte(normalText))
 	if err != nil {
 		log.Fatal((err))
+		return "", nil, enErr
 	}
 	fmt.Printf("encoded password:\t %v \n", encoded)
-	// save format for db or json
-	// db.Set([]byte(key), []byte(base64.StdEncoding.EncodeToString(encoded)))
-	//fmt.Printf("base 64 encoded password:\t %v \n", []byte(base64.StdEncoding.EncodeToString(encoded)))
+	return string(encoded), privKey, nil
+}
 
-	//for decryption now all steps in reverse:
-	unwrapped, err := decryptWithPrivateKey(privKey, encoded)
+func decrypt(encryptedText []byte, pemPrivKey string) string {
+
+	// Deserialize the string back to a private key
+		deserializedKey, err := StringToPrivateKey(pemPrivKey)
+		if err != nil {
+			log.Fatal((err))
+			return ""
+		}
+
+	unwrapped, err := decryptWithPrivateKey(deserializedKey, encryptedText)
 	if err != nil {
 		log.Fatal((err))
+		return ""
 	}
-	/*
-	var unwrap bytes.Buffer
-	unwrap.WriteString(string(unwrapped))
-	decrypted, err := decryptBox(unwrap)
+	return string(unwrapped)
+}
+
+func main() {
+	passwords := []string {"passwordForTwitch123", "passwordForDiscord456", "passwordForTwitter789", "passwordForFFIV012", "passwordForPayPal345", "passwordForMyCompany678", "passwordForExpo901"}
+
+	encryptedPasswords := []string {}
+
+	privateKeys := []string {}
+
+	for _, password := range passwords {
+		encrypted, privateKey, _ := encrypt(password)
+		encryptedPasswords = append(encryptedPasswords, encrypted)
+
+		// Serialize the private key to a string
+		pemString, err := PrivateKeyToString(privateKey)
+		if err != nil {
+			fmt.Println("Error converting private key to string:", err)
+			return
+		}
+
+		privateKeys = append(privateKeys, pemString)
+	}
+
+	fmt.Println("Elements in encrypted passwords:")
+	for _, str := range encryptedPasswords {
+		fmt.Println(str)
+	}
+
+	fmt.Println("/////////////////////////////////////////////////////////////")
+
+	fmt.Println("Elements in private keys:")
+	for _, str := range privateKeys {
+		fmt.Println(str)
+	}
+
+	data := make(map[string]User)
+
+	for index, str := range passwords {
+		u := new(User)
+    u.Password = str
+		u.EncryptedPassword = encryptedPasswords[index]
+		u.PrivateKey = privateKeys[index]
+		data[string(index)] = User{Password: u.Password, EncryptedPassword: u.EncryptedPassword, PrivateKey: u.PrivateKey}
+
+	}
+
+	// Convert the data to JSON format
+	jsonData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		log.Fatal((err))
+		fmt.Println("Error creating JSON:", err)
 	}
-	*/
-	fmt.Printf("here is your original password:\t %v \n", string(unwrapped))
+
+	// Save JSON to a file
+	savePath := "users.json"
+	err = os.WriteFile(savePath, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON to file:", err)
+		return
+	}
+
+	fmt.Printf("User credentials saved to %s\n", savePath)
 
 }
 
