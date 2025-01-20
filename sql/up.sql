8186d48ed46856199dfb87f305873b8dde3baaab
stark5200 <a.taha5200@gmail.com>
2025-01-20 07:19:04 +0100

    select sql as

--- sql/up.sql
+++ sql/up.sql
@@ -6,3 +6,29 @@ CREATE TABLE people (
     balance INTEGER,
     is_admin BOOLEAN
 );
+
+CREATE TABLE banks (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    location TEXT NOT NULL,
+    routing_number INTEGER NOT NULL
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    country_code TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    is_admin BOOLEAN,
+    bank_id INTEGER, 
+    CONSTRAINT fk_banks
+    FOREIGN KEY (bank_id)
+    REFERENCES banks(id)
+);
+
+-- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
+
+SELECT sql AS users_table FROM sqlite_schema WHERE type='table' AND name='users';
+
