bb6fa90bcd19105fc3d5c7a166e012dec5e7d7cf
stark5200 <a.taha5200@gmail.com>
2025-02-24 22:49:57 +0100

    users_countries

--- sql/main.sql
+++ sql/main.sql
@@ -186,6 +186,28 @@ CREATE TABLE countries (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  is_admin BOOLEAN
+);
+
+CREATE TABLE countries (
+  id INTEGER PRIMARY KEY,
+  country_code TEXT,
+  name TEXT
+);
+
+CREATE TABLE users_countries (
+  country_id INTEGER,
+  user_id INTEGER,
+  UNIQUE(country_id, user_id)
+);
+
+
 SELECT round(avg(age)) AS round_age
 FROM users
 WHERE country_code = 'US';
