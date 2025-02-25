38284f54eb03f372007935f553c231bf3df874ef
stark5200 <a.taha5200@gmail.com>
2025-02-25 23:47:37 +0100

    3nf

--- sql/main.sql
+++ sql/main.sql
@@ -212,6 +212,32 @@ SELECT round(avg(age)) AS round_age
 FROM users
 WHERE country_code = 'US';
 
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  num_employees INTEGER NOT NULL
+);
+
+-- Don't touch between these comments --
+
+INSERT INTO companies(name, num_employees)
+  VALUES ('Pfizer', 10000);
+INSERT INTO companies(name, num_employees)
+  VALUES ('WorldBanc', 80);
+INSERT INTO companies(name, num_employees)
+  VALUES ('Fantasy Quest', 30);
+INSERT INTO companies(name, num_employees)
+  VALUES ('Walmart', 1000);
+
+-- Don't touch between these comments --
+
+SELECT *,
+  CASE 
+    WHEN num_employees >= 100 THEN 'Large'
+    ELSE 'Small'
+  END AS size
+FROM companies;
+
 
 
 
