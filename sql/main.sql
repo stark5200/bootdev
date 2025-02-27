e17a3b7ac70f9788e9466395314bc91018582d88
stark5200 <a.taha5200@gmail.com>
2025-02-27 05:05:54 +0100

    order countries

--- sql/main.sql
+++ sql/main.sql
@@ -268,3 +268,9 @@ SELECT *
 FROM users
 INNER JOIN countries
 ON countries.country_code = users.country_code;
+
+SELECT users.name, users.age, countries.name AS country_name
+FROM users
+INNER JOIN countries ON countries.country_code = users.country_code
+ORDER BY country_name;
+
