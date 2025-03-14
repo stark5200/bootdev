c4b8a7c280fd3c9b90bc75bf387121558823ac07
stark5200 <a.taha5200@gmail.com>
2025-03-14 23:37:55 +0100

    nullish

--- javascript/boot.js
+++ javascript/boot.js
@@ -517,3 +517,14 @@ if (userCredits >= 1) {
 } else {
   console.log("Not enough credits.");
 }
+
+const name = "James Holden";
+const provider = "AT&T";
+const phoneNumber = "555-123-4567";
+const subscriptionType = null;
+
+// don't touch above this line
+
+console.log(
+  `Creating ${subscriptionType ?? "Guest"} Profile for ${name} with ${provider} at ${phoneNumber}.`,
+);
