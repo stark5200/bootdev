e4524b3c9f65fb1d2a4c913aa726957d0399815e
stark5200 <a.taha5200@gmail.com>
2025-03-20 23:58:05 +0100

    js comments

--- javascript/boot.js
+++ javascript/boot.js
@@ -557,3 +557,15 @@ if (isBirthday) {
 console.log("sending welcome message...");
 console.log("messageText: ", messageText);
 
+// Attention!
+/*
+  We are increasing the maximum message length from 140 to 280 characters.
+  Very reluctantly, I might add.
+  Users actually want to write more than 140 characters?!? Madness.
+*
+
+const maxMessageLength = 140;
+const newMaxMessageLength = 280;
+console.log(
+  `Textio is increasing the maximum message length from ${maxMessageLength} to ${newMaxMessageLength} characters.`,
+);
