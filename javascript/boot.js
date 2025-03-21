fa0fd485b8d7da6455ac571e9e6b094931e32095
stark5200 <a.taha5200@gmail.com>
2025-03-21 23:44:08 +0100

    fixcomment

--- javascript/boot.js
+++ javascript/boot.js
@@ -562,10 +562,12 @@ console.log("messageText: ", messageText);
   We are increasing the maximum message length from 140 to 280 characters.
   Very reluctantly, I might add.
   Users actually want to write more than 140 characters?!? Madness.
-*
+*/
 
 const maxMessageLength = 140;
 const newMaxMessageLength = 280;
 console.log(
   `Textio is increasing the maximum message length from ${maxMessageLength} to ${newMaxMessageLength} characters.`,
 );
+
+
