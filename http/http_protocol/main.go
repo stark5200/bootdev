c887e231b2afff4186545ea50f9bcc8b2ad81876
stark5200 <a.taha5200@gmail.com>
2025-03-26 23:57:55 +0100

    some solutions

--- http/http_protocol/main.go
+++ http/http_protocol/main.go
@@ -11,7 +11,33 @@ import (
 )
 
 /*
+const inputFilePath = "messages.txt"
+// read 8 byte solution
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer f.Close()
 
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	for {
+		b := make([]byte, 8, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
+}
+// read 8 byte solution
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
