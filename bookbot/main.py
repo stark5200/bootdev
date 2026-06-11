1d8329d91c66578e9021cbb77e3ab6247902c079
stark5200 <a.taha5200@gmail.com>
2026-06-12 01:18:26 +0200

    bookbot done

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,17 +1,35 @@
-from stats import count_words, count_unique_chars
-
-book_path = "books/frankenstein.txt"    
+import sys
+from stats import count_words, count_unique_chars, chars_dict_to_sorted_list
     
 def get_book_text(book_path):
   with open(book_path) as f:
     file_contents = f.read()
   return file_contents
 
+def print_report(book_path, word_count, sorted_list):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book_path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+  print("--------- Character Count -------")
+  for char, count in sorted_list:
+    if char.isalpha():
+      print(f"{char}: {count}")
+  print("============= END ===============")
+
+  
+
 def main():
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  
+  book_path = sys.argv[1]
   book_text = get_book_text(book_path)
   word_count = count_words(book_text)
-  print(f"Found {word_count} total words") # Print the word count
   unique_char_count = count_unique_chars(book_text)
-  print(unique_char_count) # Print the unique character count
+  #print(unique_char_count) # Print the unique character count
+  sorted_list = chars_dict_to_sorted_list(unique_char_count)
+  print_report(book_path, word_count, sorted_list)
 
 main()
--- bookbot/stats.py
+++ bookbot/stats.py
@@ -8,3 +8,13 @@ def count_unique_chars(text):
   for char in chars:
     char_count[char] = text.lower().count(char)
   return char_count
+
+def sort_on(char: tuple[str, int]):
+  return char[1]
+
+def chars_dict_to_sorted_list(char_count_dict):
+  char_count_list = []
+  for char, count in char_count_dict.items():
+    char_count_list.append((char, count))
+  char_count_list = sorted(char_count_list, key=sort_on, reverse=True)
+  return char_count_list
